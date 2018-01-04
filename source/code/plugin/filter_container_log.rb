3e452ad2931f94a7f41435a0c6fa3b5ccd1932aa
Sambit Mishra <samis@microsoft.com>
2018-01-03 18:40:14 -0800

    Adding tail plugin

---
+++ source/code/plugin/filter_container_log.rb
@@ -0,0 +1,33 @@
+require 'fluent/filter'
+
+module Fluent
+  require 'logger'
+  class PassThruFilter < Filter
+    Fluent::Plugin.register_filter('filter_container_log', self)
+
+    def configure(conf)
+      super
+      require_relative 'omslog'
+      @containerCache = Hash.new
+    end
+
+    def start
+      super
+      @hostname = OMS::Common.get_hostname or "Unknown host"
+    end
+
+    def shutdown
+      super
+    end
+    
+    def filter(tag, time, record)
+      record['Computer'] =  @hostname
+      wrapper = {
+                 "DataType"=>"CONTAINER_LOG_BLOB",
+                 "IPName"=>"Containers",
+                 "DataItems"=>[record.each{|k,v| record[k]=v}]
+      }
+      wrapper
+    end
+  end
+end
