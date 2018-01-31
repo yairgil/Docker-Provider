76ef10758a64eb1798e6c1307f4df2c717805b6d
Sambit Mishra <samis@microsoft.com>
2018-01-31 12:33:47 -0800

    Inital commit for KubeNodeInventory_CL and node allocatable metrics

---
+++ source/code/plugin/in_kube_nodes.rb
@@ -0,0 +1,118 @@
+#!/usr/local/bin/ruby
+
+module Fluent
+
+    class Kube_nodeInventory_Input < Input
+      Plugin.register_input('kubenodeinventory', self)
+  
+      def initialize
+        super
+        require 'yaml'
+        require 'json'
+  
+        require_relative 'KubernetesApiClient'
+        require_relative 'oms_common'
+        require_relative 'omslog'
+      end
+  
+      config_param :run_interval, :time, :default => '10m'
+      config_param :tag, :string, :default => "oms.api.KubeNodeInventory"
+  
+      def configure (conf)
+        super
+      end
+  
+      def start
+        if KubernetesApiClient.isValidRunningNode && @run_interval
+          @finished = false
+          @condition = ConditionVariable.new
+          @mutex = Mutex.new
+          @thread = Thread.new(&method(:run_periodic))
+        else
+          enumerate
+        end
+      end
+  
+      def shutdown
+        if KubernetesApiClient.isValidRunningNode && @run_interval
+          @mutex.synchronize {
+            @finished = true
+            @condition.signal
+          }
+          @thread.join
+        end
+      end
+  
+      def enumerate
+        time = Time.now.to_f
+        if KubernetesApiClient.isValidRunningNode
+          nodeInventory = JSON.parse(KubernetesApiClient.getKubeResourceInfo('nodes').body)
+          begin
+            if(!nodeInventory.empty?)
+
+                #get allocatable limits per node as perf data
+                #<TODO> Node capacity is different from node allocatable. Allocatable is what is avaialble for allocating pods.
+                # In theory Capacity = Allocatable + kube-reserved + system-reserved + eviction-threshold
+                # For more details refer to https://kubernetes.io/docs/tasks/administer-cluster/reserve-compute-resources/#node-allocatable 
+                metricDataItems = []
+                metricDataItems.concat(KubernetesApiClient.parseNodeLimits(nodeInventory, "allocatable", "cpu", "cpuAllocatableNanoCores"))
+                metricDataItems.concat(KubernetesApiClient.parseNodeLimits(nodeInventory, "allocatable", "memory", "memoryAllocatableBytes"))
+
+                metricDataItems.each do |record|
+                record['DataType'] = "LINUX_PERF_BLOB"
+                record['IPName'] = "LogManagement"
+                router.emit("oms.api.KubePerf", time, record) if record  
+                end  
+
+                #get node inventory 
+                nodeInventory['items'].each do |items|
+                    record = {}
+                    record['Computer'] = items['metadata']['name']   
+                    record['CreationTimeStamp'] = items['metadata']['creationTimestamp'] 
+                    record['Labels'] = [items['metadata']['labels']]
+                    record['Status'] = ""
+
+                    # Refer to https://kubernetes.io/docs/concepts/architecture/nodes/#condition for possible node conditions.
+                    # We check the status of each condition e.g. {"type": "OutOfDisk","status": "False"} . Based on this we 
+                    # populate the KubeNodeInventory Status field. A possible value for this field could be "Ready OutofDisk"
+                    # implying that the node is ready for hosting pods, however its out of disk.
+                    
+                    items['status']['conditions'].each do |condition|
+                        if condition['status'] == "True"
+                            record['Status'] += condition['type']
+                        end 
+                    end 
+                    
+                    record['KubeletVersion'] = items['status']['nodeInfo']['kubeletVersion']
+                    record['KubeProxyVersion'] = items['status']['nodeInfo']['kubeProxyVersion']
+                    router.emit(@tag, time, record)
+                end 
+    
+            end  
+          rescue  => errorStr
+            $log.warn "Failed to retrieve node inventory: #{errorStr}"
+            $log.debug_backtrace(errorStr.backtrace)
+          end       
+        end
+      end
+  
+      def run_periodic
+        @mutex.lock
+        done = @finished
+        until done
+          @condition.wait(@mutex, @run_interval)
+          done = @finished
+          @mutex.unlock
+          if !done
+            enumerate
+          end
+          @mutex.lock
+        end
+        @mutex.unlock
+      end
+  
+    end # Kube_Node_Input
+  
+  end # module
+  
+  
