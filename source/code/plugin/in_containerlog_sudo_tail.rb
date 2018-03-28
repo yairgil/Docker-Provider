63680d1194df9caac5770025f971aeb2cd343b6a
bragi92 <kadubey@microsoft.com>
2018-03-28 09:56:40 -0700

    Add TODO comment

--- source/code/plugin/in_containerlog_sudo_tail.rb
+++ source/code/plugin/in_containerlog_sudo_tail.rb
@@ -20,6 +20,7 @@ module Fluent
       #We have read permission on this file but don't have execute permission on the below mentioned path. Hence wildcard character searches to find the container ID's doesn't work.	
       @containerLogFilePath = "/var/lib/docker/containers/"
       #This folder contains a list of all the containers running/stopped and we're using it to get all the container ID's which will be needed for the log file path below
+      #TODO : Use generic path from docker REST endpoint and find a way to mount the correct folder in the omsagent.yaml	    
       @containerIDFilePath = "/var/opt/microsoft/docker-cimprov/state/ContainerInventory/*"
     end
 
