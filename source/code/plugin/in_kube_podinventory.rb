96c546eefc0acb2e8aaaa05fbc7555725905b731
Vishwanath Narasimhan <visnara@microsoft.com>
2018-04-18 09:54:56 -0700

    Guard/Handle errors from enumerate

--- source/code/plugin/in_kube_podinventory.rb
+++ source/code/plugin/in_kube_podinventory.rb
@@ -196,8 +196,12 @@ module Fluent
         done = @finished
         @mutex.unlock
         if !done
-          $log.info("in_kube_podinventory::run_periodic @ #{Time.now.utc.iso8601}")
-          enumerate
+          begin
+            $log.info("in_kube_podinventory::run_periodic @ #{Time.now.utc.iso8601}")
+            enumerate
+          rescue => errorStr
+            $log.warn "in_kube_podinventory::run_periodic: enumerate Failed to retrieve pod inventory: #{errorStr}"
+          end
         end
         @mutex.lock
       end
