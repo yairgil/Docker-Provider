3879d233e9f607902ebe2adbd99c996a9db8ba1f
Dilip Raghunathan <dilip.rangarajan@gmail.com>
2018-09-14 10:46:55 -0700

    Dilipr/fluent bit log processing (#126)
    * Build out_oms.so and include in docker-cimprov package
    
    * Adding fluent-bit-config file to base container
    
    * PR Feedback
    
    * Adding out_oms.conf to base_container.data
    
    * PR Feedback
    
    * Making the critical section as small as possible
    
    * PR Feedback
    
    * Fixing the newline bug for Computer, and changing containerId to Id

---
+++ source/code/go/src/plugins/out_oms.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"github.com/fluent/fluent-bit-go/output"
+)
+import (
+	"C"
+	"unsafe"
+)
+
+//export FLBPluginRegister
+func FLBPluginRegister(ctx unsafe.Pointer) int {
+	return output.FLBPluginRegister(ctx, "oms", "Stdout GO!")
+}
+
+//export FLBPluginInit
+// (fluentbit will call this)
+// ctx (context) pointer to fluentbit context (state/ c code)
+func FLBPluginInit(ctx unsafe.Pointer) int {
+	Log("Initializing out_oms go plugin for fluentbit")
+	PluginConfiguration = ReadConfig("/etc/opt/microsoft/docker-cimprov/out_oms.conf")
+	CreateHTTPClient()
+	updateContainersData()
+	return output.FLB_OK
+}
+
+//export FLBPluginFlush
+func FLBPluginFlush(data unsafe.Pointer, length C.int, tag *C.char) int {
+	var count int
+	var ret int
+	var record map[interface{}]interface{}
+	var records []map[interface{}]interface{}
+
+	// Create Fluent Bit decoder
+	dec := output.NewDecoder(data, int(length))
+
+	// Iterate Records
+	count = 0
+	for {
+		// Extract Record
+		ret, _, record = output.GetRecord(dec)
+		if ret != 0 {
+			break
+		}
+		records = append(records, record)
+		count++
+	}
+	return PostDataHelper(records)
+}
+
+// FLBPluginExit exits the plugin
+func FLBPluginExit() int {
+	return output.FLB_OK
+}
+
+func main() {
+}
