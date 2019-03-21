f1325de25dd3cebdc4cb5d2a2e81fcc77a1f76cd
Vishwanath Narasimhan <visnara@microsoft.com>
2019-03-21 16:38:51 -0700

    telemetry & other changes

--- installer/scripts/Telegraf403Telemetry.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-countErr=$(grep -iF  "Error writing to output [socket_writer]" /var/opt/microsoft/docker-cimprov/log/telegraf.log | wc -l | tr -d '\n')
-echo "telegraf,AKS_RESOURCE_ID=${AKS_RESOURCE_ID} telegrafTCPWriteErrorCountTotal=${countErr}i"
