6953f50a62c7faade0db553e0839f137b252309b
rashmichandrashekar <rashmy@microsoft.com>
2019-04-01 14:48:19 -0700

    Rashmi/win nodepool - PR (#206)
    * changes for win nodes enumeration
    
    * changes
    
    * changes
    
    * changes
    
    * node cpu metric rate changes
    
    * container cpu rate
    
    * changes
    
    * changes
    
    * changes
    
    * changes
    
    * changes
    
    * changes to include in_win_cadvisor_perf.rb file
    
    * send containerinventoryheartbeatevent
    
    * changes
    
    * cahnges for mdm metrics
    
    * changes
    
    * cahnges
    
    * changes
    
    * container states
    
    * changes
    
    * changes
    
    * changes for env variables
    
    * changes
    
    * changes
    
    * changes
    
    * changes
    
    * delete comments
    
    * changes
    
    * mutex changes
    
    * changes
    
    * changes
    
    * changes
    
    * telemetry fix for docker version
    
    * removing hardcoded values for mdm
    
    * update docker version
    
    * telemetry for windows cadvisor timeouts
    
    * exeception key update to computer
    
    * PR comments

--- source/code/plugin/in_kube_nodes.rb
+++ source/code/plugin/in_kube_nodes.rb
@@ -2,181 +2,176 @@
 # frozen_string_literal: true
 
 module Fluent
+  class Kube_nodeInventory_Input < Input
+    Plugin.register_input("kubenodeinventory", self)
 
-    class Kube_nodeInventory_Input < Input
-      Plugin.register_input('kubenodeinventory', self)
-  
-      @@ContainerNodeInventoryTag = 'oms.containerinsights.ContainerNodeInventory'
-      @@MDMKubeNodeInventoryTag = 'mdm.kubenodeinventory'
+    @@ContainerNodeInventoryTag = "oms.containerinsights.ContainerNodeInventory"
+    @@MDMKubeNodeInventoryTag = "mdm.kubenodeinventory"
 
-      def initialize
-        super
-        require 'yaml'
-        require 'json'
-  
-        require_relative 'KubernetesApiClient'
-        require_relative 'ApplicationInsightsUtility'
-        require_relative 'oms_common'
-        require_relative 'omslog'
+    def initialize
+      super
+      require "yaml"
+      require "json"
 
+      require_relative "KubernetesApiClient"
+      require_relative "ApplicationInsightsUtility"
+      require_relative "oms_common"
+      require_relative "omslog"
+    end
+
+    config_param :run_interval, :time, :default => "1m"
+    config_param :tag, :string, :default => "oms.containerinsights.KubeNodeInventory"
+
+    def configure(conf)
+      super
+    end
+
+    def start
+      if @run_interval
+        @finished = false
+        @condition = ConditionVariable.new
+        @mutex = Mutex.new
+        @thread = Thread.new(&method(:run_periodic))
+        @@nodeTelemetryTimeTracker = DateTime.now.to_time.to_i
       end
-  
-      config_param :run_interval, :time, :default => '1m'
-      config_param :tag, :string, :default => "oms.containerinsights.KubeNodeInventory"
-  
-      def configure (conf)
-        super
+    end
+
+    def shutdown
+      if @run_interval
+        @mutex.synchronize {
+          @finished = true
+          @condition.signal
+        }
+        @thread.join
       end
-  
-      def start
-        if @run_interval
-          @finished = false
-          @condition = ConditionVariable.new
-          @mutex = Mutex.new
-          @thread = Thread.new(&method(:run_periodic))
-          @@nodeTelemetryTimeTracker = DateTime.now.to_time.to_i
-        end
-      end
-  
-      def shutdown
-        if @run_interval
-          @mutex.synchronize {
-            @finished = true
-            @condition.signal
-          }
-          @thread.join
-        end
-      end
-  
-      def enumerate
-        currentTime = Time.now
-        emitTime = currentTime.to_f
-        batchTime = currentTime.utc.iso8601
-        telemetrySent = false
-        $log.info("in_kube_nodes::enumerate : Getting nodes from Kube API @ #{Time.now.utc.iso8601}")
-        nodeInventory = JSON.parse(KubernetesApiClient.getKubeResourceInfo('nodes').body)
-        $log.info("in_kube_nodes::enumerate : Done getting nodes from Kube API @ #{Time.now.utc.iso8601}")
-          begin
-            if(!nodeInventory.empty?)
-              eventStream = MultiEventStream.new
-              containerNodeInventoryEventStream = MultiEventStream.new 
-                #get node inventory 
-                nodeInventory['items'].each do |items|
-                    record = {}
-                    # Sending records for ContainerNodeInventory
-                    containerNodeInventoryRecord = {}
-                    containerNodeInventoryRecord['CollectionTime'] = batchTime #This is the time that is mapped to become TimeGenerated
-                    containerNodeInventoryRecord['Computer'] = items['metadata']['name']
+    end
 
-                    record['CollectionTime'] = batchTime #This is the time that is mapped to become TimeGenerated
-                    record['Computer'] = items['metadata']['name'] 
-                    record['ClusterName'] = KubernetesApiClient.getClusterName
-                    record['ClusterId'] = KubernetesApiClient.getClusterId  
-                    record['CreationTimeStamp'] = items['metadata']['creationTimestamp'] 
-                    record['Labels'] = [items['metadata']['labels']]
-                    record['Status'] = ""
+    def enumerate
+      currentTime = Time.now
+      emitTime = currentTime.to_f
+      batchTime = currentTime.utc.iso8601
+      telemetrySent = false
+      $log.info("in_kube_nodes::enumerate : Getting nodes from Kube API @ #{Time.now.utc.iso8601}")
+      nodeInventory = JSON.parse(KubernetesApiClient.getKubeResourceInfo("nodes").body)
+      $log.info("in_kube_nodes::enumerate : Done getting nodes from Kube API @ #{Time.now.utc.iso8601}")
+      begin
+        if (!nodeInventory.empty?)
+          eventStream = MultiEventStream.new
+          containerNodeInventoryEventStream = MultiEventStream.new
+          #get node inventory
+          nodeInventory["items"].each do |items|
+            record = {}
+            # Sending records for ContainerNodeInventory
+            containerNodeInventoryRecord = {}
+            containerNodeInventoryRecord["CollectionTime"] = batchTime #This is the time that is mapped to become TimeGenerated
+            containerNodeInventoryRecord["Computer"] = items["metadata"]["name"]
 
-                    # Refer to https://kubernetes.io/docs/concepts/architecture/nodes/#condition for possible node conditions.
-                    # We check the status of each condition e.g. {"type": "OutOfDisk","status": "False"} . Based on this we 
-                    # populate the KubeNodeInventory Status field. A possible value for this field could be "Ready OutofDisk"
-                    # implying that the node is ready for hosting pods, however its out of disk.
-                    
-                    if items['status'].key?("conditions") && !items['status']['conditions'].empty?
-                      allNodeConditions="" 
-                      items['status']['conditions'].each do |condition|
-                          if condition['status'] == "True"
-                            if !allNodeConditions.empty?
-                              allNodeConditions = allNodeConditions + "," + condition['type']
-                            else
-                              allNodeConditions = condition['type']
-                            end
-                          end 
-                          #collect last transition to/from ready (no matter ready is true/false)
-                          if condition['type'] == "Ready" && !condition['lastTransitionTime'].nil?
-                            record['LastTransitionTimeReady'] = condition['lastTransitionTime']
-                          end
-                      end 
-                      if !allNodeConditions.empty?
-                        record['Status'] = allNodeConditions
-                      end
+            record["CollectionTime"] = batchTime #This is the time that is mapped to become TimeGenerated
+            record["Computer"] = items["metadata"]["name"]
+            record["ClusterName"] = KubernetesApiClient.getClusterName
+            record["ClusterId"] = KubernetesApiClient.getClusterId
+            record["CreationTimeStamp"] = items["metadata"]["creationTimestamp"]
+            record["Labels"] = [items["metadata"]["labels"]]
+            record["Status"] = ""
 
-                    end
+            # Refer to https://kubernetes.io/docs/concepts/architecture/nodes/#condition for possible node conditions.
+            # We check the status of each condition e.g. {"type": "OutOfDisk","status": "False"} . Based on this we
+            # populate the KubeNodeInventory Status field. A possible value for this field could be "Ready OutofDisk"
+            # implying that the node is ready for hosting pods, however its out of disk.
 
-                    nodeInfo = items['status']['nodeInfo']
-                    record['KubeletVersion'] = nodeInfo['kubeletVersion']
-                    record['KubeProxyVersion'] = nodeInfo['kubeProxyVersion']
-                    containerNodeInventoryRecord['OperatingSystem'] = nodeInfo['osImage']
-                    dockerVersion = nodeInfo['containerRuntimeVersion']
-                    dockerVersion.slice! "docker://"
-                    containerNodeInventoryRecord['DockerVersion'] = dockerVersion
-                    # ContainerNodeInventory data for docker version and operating system.
-                    containerNodeInventoryWrapper = {
-                      "DataType"=>"CONTAINER_NODE_INVENTORY_BLOB",
-                      "IPName"=>"ContainerInsights",
-                      "DataItems"=>[containerNodeInventoryRecord.each{|k,v| containerNodeInventoryRecord[k]=v}]
-                    }
-                    containerNodeInventoryEventStream.add(emitTime, containerNodeInventoryWrapper) if containerNodeInventoryWrapper
-
-                    wrapper = {
-                      "DataType"=>"KUBE_NODE_INVENTORY_BLOB",
-                      "IPName"=>"ContainerInsights",
-                      "DataItems"=>[record.each{|k,v| record[k]=v}]
-                    }
-                    eventStream.add(emitTime, wrapper) if wrapper
-                    # Adding telemetry to send node telemetry every 5 minutes
-                    timeDifference =  (DateTime.now.to_time.to_i - @@nodeTelemetryTimeTracker).abs
-                    timeDifferenceInMinutes = timeDifference/60
-                    if (timeDifferenceInMinutes >= 5)
-                      properties = {}
-                      properties["Computer"] = record["Computer"]
-                      properties["KubeletVersion"] = record["KubeletVersion"]
-                      capacityInfo = items['status']['capacity']
-                      ApplicationInsightsUtility.sendMetricTelemetry("NodeCoreCapacity", capacityInfo["cpu"] , properties)
-                      ApplicationInsightsUtility.sendMetricTelemetry("NodeMemory", capacityInfo["memory"] , properties)
-                      telemetrySent = true
-                    end
-                end 
-                router.emit_stream(@tag, eventStream) if eventStream
-                router.emit_stream(@@MDMKubeNodeInventoryTag, eventStream) if eventStream
-                router.emit_stream(@@ContainerNodeInventoryTag, containerNodeInventoryEventStream) if containerNodeInventoryEventStream
-                if telemetrySent == true
-                  @@nodeTelemetryTimeTracker = DateTime.now.to_time.to_i
+            if items["status"].key?("conditions") && !items["status"]["conditions"].empty?
+              allNodeConditions = ""
+              items["status"]["conditions"].each do |condition|
+                if condition["status"] == "True"
+                  if !allNodeConditions.empty?
+                    allNodeConditions = allNodeConditions + "," + condition["type"]
+                  else
+                    allNodeConditions = condition["type"]
+                  end
                 end
-                @@istestvar = ENV['ISTEST']
-                if (!@@istestvar.nil? && !@@istestvar.empty? && @@istestvar.casecmp('true') == 0 && eventStream.count > 0)
-                  $log.info("kubeNodeInventoryEmitStreamSuccess @ #{Time.now.utc.iso8601}")
+                #collect last transition to/from ready (no matter ready is true/false)
+                if condition["type"] == "Ready" && !condition["lastTransitionTime"].nil?
+                  record["LastTransitionTimeReady"] = condition["lastTransitionTime"]
                 end
-            end  
-          rescue  => errorStr
-            $log.warn "Failed to retrieve node inventory: #{errorStr}"
-            $log.debug_backtrace(errorStr.backtrace)
-            ApplicationInsightsUtility.sendExceptionTelemetry(errorStr)
-          end       
-      end
-  
-      def run_periodic
-        @mutex.lock
-        done = @finished
-        until done
-          @condition.wait(@mutex, @run_interval)
-          done = @finished
-          @mutex.unlock
-          if !done
-            begin
-              $log.info("in_kube_nodes::run_periodic @ #{Time.now.utc.iso8601}")
-              enumerate
-            rescue => errorStr
-              $log.warn "in_kube_nodes::run_periodic: enumerate Failed to retrieve node inventory: #{errorStr}"
-              ApplicationInsightsUtility.sendExceptionTelemetry(errorStr)
+              end
+              if !allNodeConditions.empty?
+                record["Status"] = allNodeConditions
+              end
+            end
+
+            nodeInfo = items["status"]["nodeInfo"]
+            record["KubeletVersion"] = nodeInfo["kubeletVersion"]
+            record["KubeProxyVersion"] = nodeInfo["kubeProxyVersion"]
+            containerNodeInventoryRecord["OperatingSystem"] = nodeInfo["osImage"]
+            dockerVersion = nodeInfo["containerRuntimeVersion"]
+            dockerVersion.slice! "docker://"
+            containerNodeInventoryRecord["DockerVersion"] = dockerVersion
+            # ContainerNodeInventory data for docker version and operating system.
+            containerNodeInventoryWrapper = {
+              "DataType" => "CONTAINER_NODE_INVENTORY_BLOB",
+              "IPName" => "ContainerInsights",
+              "DataItems" => [containerNodeInventoryRecord.each { |k, v| containerNodeInventoryRecord[k] = v }],
+            }
+            containerNodeInventoryEventStream.add(emitTime, containerNodeInventoryWrapper) if containerNodeInventoryWrapper
+
+            wrapper = {
+              "DataType" => "KUBE_NODE_INVENTORY_BLOB",
+              "IPName" => "ContainerInsights",
+              "DataItems" => [record.each { |k, v| record[k] = v }],
+            }
+            eventStream.add(emitTime, wrapper) if wrapper
+            # Adding telemetry to send node telemetry every 5 minutes
+            timeDifference = (DateTime.now.to_time.to_i - @@nodeTelemetryTimeTracker).abs
+            timeDifferenceInMinutes = timeDifference / 60
+            if (timeDifferenceInMinutes >= 5)
+              properties = {}
+              properties["Computer"] = record["Computer"]
+              properties["KubeletVersion"] = record["KubeletVersion"]
+              properties["OperatingSystem"] = nodeInfo["operatingSystem"]
+              properties["DockerVersion"] = dockerVersion
+              capacityInfo = items["status"]["capacity"]
+              ApplicationInsightsUtility.sendMetricTelemetry("NodeCoreCapacity", capacityInfo["cpu"], properties)
+              ApplicationInsightsUtility.sendMetricTelemetry("NodeMemory", capacityInfo["memory"], properties)
+              telemetrySent = true
             end
           end
-          @mutex.lock
+          router.emit_stream(@tag, eventStream) if eventStream
+          router.emit_stream(@@MDMKubeNodeInventoryTag, eventStream) if eventStream
+          router.emit_stream(@@ContainerNodeInventoryTag, containerNodeInventoryEventStream) if containerNodeInventoryEventStream
+          if telemetrySent == true
+            @@nodeTelemetryTimeTracker = DateTime.now.to_time.to_i
+          end
+          @@istestvar = ENV["ISTEST"]
+          if (!@@istestvar.nil? && !@@istestvar.empty? && @@istestvar.casecmp("true") == 0 && eventStream.count > 0)
+            $log.info("kubeNodeInventoryEmitStreamSuccess @ #{Time.now.utc.iso8601}")
+          end
         end
-        @mutex.unlock
+      rescue => errorStr
+        $log.warn "Failed to retrieve node inventory: #{errorStr}"
+        $log.debug_backtrace(errorStr.backtrace)
+        ApplicationInsightsUtility.sendExceptionTelemetry(errorStr)
       end
-  
-    end # Kube_Node_Input
-  
-  end # module
-  
-  
+    end
+
+    def run_periodic
+      @mutex.lock
+      done = @finished
+      until done
+        @condition.wait(@mutex, @run_interval)
+        done = @finished
+        @mutex.unlock
+        if !done
+          begin
+            $log.info("in_kube_nodes::run_periodic @ #{Time.now.utc.iso8601}")
+            enumerate
+          rescue => errorStr
+            $log.warn "in_kube_nodes::run_periodic: enumerate Failed to retrieve node inventory: #{errorStr}"
+            ApplicationInsightsUtility.sendExceptionTelemetry(errorStr)
+          end
+        end
+        @mutex.lock
+      end
+      @mutex.unlock
+    end
+  end # Kube_Node_Input
+end # module
--- source/code/plugin/out_mdm.rb
+++ source/code/plugin/out_mdm.rb
@@ -2,29 +2,27 @@
 # frozen_string_literal: true
 
 module Fluent
-
   class OutputMDM < BufferedOutput
-
     config_param :retry_mdm_post_wait_minutes, :integer
 
-    Plugin.register_output('out_mdm', self)
+    Plugin.register_output("out_mdm", self)
 
     def initialize
       super
-      require 'net/http'
-      require 'net/https'
-      require 'uri'
-      require 'json'
-      require_relative 'KubernetesApiClient'
-      require_relative 'ApplicationInsightsUtility'
+      require "net/http"
+      require "net/https"
+      require "uri"
+      require "json"
+      require_relative "KubernetesApiClient"
+      require_relative "ApplicationInsightsUtility"
 
-      @@token_resource_url = 'https://monitoring.azure.com/'
-      @@grant_type = 'client_credentials'
-      @@azure_json_path = '/etc/kubernetes/host/azure.json'
+      @@token_resource_url = "https://monitoring.azure.com/"
+      @@grant_type = "client_credentials"
+      @@azure_json_path = "/etc/kubernetes/host/azure.json"
       @@post_request_url_template = "https://%{aks_region}.monitoring.azure.com%{aks_resource_id}/metrics"
       @@token_url_template = "https://login.microsoftonline.com/%{tenant_id}/oauth2/token"
       @@plugin_name = "AKSCustomMetricsMDM"
-      
+
       @data_hash = {}
       @token_url = nil
       @http_client = nil
@@ -50,12 +48,13 @@ module Fluent
         @can_send_data_to_mdm = false
         return
       end
-      # Handle the case where the file read fails. Send Telemetry and exit the plugin? 
+      # Handle the case where the file read fails. Send Telemetry and exit the plugin?
       @data_hash = JSON.parse(file)
-      @token_url = @@token_url_template % {tenant_id: @data_hash['tenantId']}
+      @token_url = @@token_url_template % {tenant_id: @data_hash["tenantId"]}
       @cached_access_token = get_access_token
-      aks_resource_id = ENV['AKS_RESOURCE_ID']
-      aks_region = ENV['AKS_REGION']
+      aks_resource_id = ENV["AKS_RESOURCE_ID"]
+      aks_region = ENV["AKS_REGION"]
+
       if aks_resource_id.to_s.empty?
         @log.info "Environment Variable AKS_RESOURCE_ID is not set.. "
         @can_send_data_to_mdm = false
@@ -77,7 +76,7 @@ module Fluent
 
     # get the access token only if the time to expiry is less than 5 minutes
     def get_access_token
-      if @cached_access_token.to_s.empty? || (Time.now + 5*60 > @token_expiry_time) # token is valid for 60 minutes. Refresh token 5 minutes from expiration
+      if @cached_access_token.to_s.empty? || (Time.now + 5 * 60 > @token_expiry_time) # token is valid for 60 minutes. Refresh token 5 minutes from expiration
         @log.info "Refreshing access token for out_mdm plugin.."
         token_uri = URI.parse(@token_url)
         http_access_token = Net::HTTP.new(token_uri.host, token_uri.port)
@@ -85,27 +84,27 @@ module Fluent
         token_request = Net::HTTP::Post.new(token_uri.request_uri)
         token_request.set_form_data(
           {
-            'grant_type' => @@grant_type, 
-            'client_id' => @data_hash['aadClientId'], 
-            'client_secret' => @data_hash['aadClientSecret'],
-            'resource' => @@token_resource_url
-            }
+            "grant_type" => @@grant_type,
+            "client_id" => @data_hash["aadClientId"],
+            "client_secret" => @data_hash["aadClientSecret"],
+            "resource" => @@token_resource_url,
+          }
         )
-        
+
         token_response = http_access_token.request(token_request)
-        # Handle the case where the response is not 200 
+        # Handle the case where the response is not 200
         parsed_json = JSON.parse(token_response.body)
-        @token_expiry_time = Time.now + 59*60 # set the expiry time to be ~one hour from current time
-        @cached_access_token = parsed_json['access_token']
+        @token_expiry_time = Time.now + 59 * 60 # set the expiry time to be ~one hour from current time
+        @cached_access_token = parsed_json["access_token"]
       end
       @cached_access_token
-    end 
+    end
 
     def write_status_file(success, message)
-      fn = '/var/opt/microsoft/omsagent/log/MDMIngestion.status'
+      fn = "/var/opt/microsoft/omsagent/log/MDMIngestion.status"
       status = '{ "operation": "MDMIngestion", "success": "%s", "message": "%s" }' % [success, message]
       begin
-        File.open(fn,'w') { |file| file.write(status) }
+        File.open(fn, "w") { |file| file.write(status) }
       rescue => e
         @log.debug "Error:'#{e}'"
         ApplicationInsightsUtility.sendExceptionTelemetry(e.backtrace)
@@ -123,13 +122,13 @@ module Fluent
       end
     end
 
-    # This method is called every flush interval. Send the buffer chunk to MDM. 
+    # This method is called every flush interval. Send the buffer chunk to MDM.
     # 'chunk' is a buffer chunk that includes multiple formatted records
     def write(chunk)
       begin
-        if (!@first_post_attempt_made || (Time.now > @last_post_attempt_time + retry_mdm_post_wait_minutes*60)) && @can_send_data_to_mdm
+        if (!@first_post_attempt_made || (Time.now > @last_post_attempt_time + retry_mdm_post_wait_minutes * 60)) && @can_send_data_to_mdm
           post_body = []
-          chunk.msgpack_each {|(tag, record)|
+          chunk.msgpack_each { |(tag, record)|
             post_body.push(record.to_json)
           }
           send_to_mdm post_body
@@ -137,7 +136,7 @@ module Fluent
           if !@can_send_data_to_mdm
             @log.info "Cannot send data to MDM since all required conditions were not met"
           else
-            @log.info "Last Failed POST attempt to MDM was made #{((Time.now - @last_post_attempt_time)/60).round(1)} min ago. This is less than the current retry threshold of #{@retry_mdm_post_wait_minutes} min. NO-OP"
+            @log.info "Last Failed POST attempt to MDM was made #{((Time.now - @last_post_attempt_time) / 60).round(1)} min ago. This is less than the current retry threshold of #{@retry_mdm_post_wait_minutes} min. NO-OP"
           end
         end
       rescue Exception => e
@@ -146,12 +145,12 @@ module Fluent
       end
     end
 
-    def send_to_mdm(post_body) 
+    def send_to_mdm(post_body)
       begin
         access_token = get_access_token
         request = Net::HTTP::Post.new(@post_request_uri.request_uri)
-        request['Content-Type'] = "application/x-ndjson"
-        request['Authorization'] = "Bearer #{access_token}"
+        request["Content-Type"] = "application/x-ndjson"
+        request["Authorization"] = "Bearer #{access_token}"
         request.body = post_body.join("\n")
         response = @http_client.request(request)
         response.value # this throws for non 200 HTTP response code
@@ -166,10 +165,10 @@ module Fluent
           @first_post_attempt_made = true
           ApplicationInsightsUtility.sendExceptionTelemetry(e.backtrace)
           # Not raising exception, as that will cause retries to happen
-        elsif !response.code.empty? && response.code.start_with?('4')
+        elsif !response.code.empty? && response.code.start_with?("4")
           # Log 400 errors and continue
           @log.info "Non-retryable HTTPServerException when POSTing Metrics to MDM #{e} Response: #{response}"
-        else 
+        else
           # raise if the response code is non-400
           @log.info "HTTPServerException when POSTing Metrics to MDM #{e} Response: #{response}"
           raise e
@@ -186,7 +185,8 @@ module Fluent
         raise e
       end
     end
-  private
+
+    private
 
     class ChunkErrorHandler
       include Configurable
@@ -218,20 +218,20 @@ module Fluent
       end
 
       def write(chunk)
-        chunk.msgpack_each {|(tag, record)|
+        chunk.msgpack_each { |(tag, record)|
           @error_handlers[tag].emit(record)
         }
       end
-   
-    private
+
+      private
 
       def create_error_handlers(router)
         nop_handler = NopErrorHandler.new
         Hash.new() { |hash, tag|
           etag = OMS::Common.create_error_tag tag
           hash[tag] = router.match?(etag) ?
-                      ErrorHandler.new(router, etag) :
-                      nop_handler
+            ErrorHandler.new(router, etag) :
+            nop_handler
         }
       end
 
@@ -251,10 +251,6 @@ module Fluent
           # NOP
         end
       end
-
     end
-
   end # class OutputMDM
-
 end # module Fluent
-
