8cdf72437b3af7b49e6931602a2f2218deea8fbe
Vishwanath <visnara@microsoft.com>
2019-04-17 19:20:57 -0700

    Fix telemetry error for telegraf err count metric (#215)

--- installer/scripts/TelegrafTCPErrorTelemetry.sh
+++ installer/scripts/TelegrafTCPErrorTelemetry.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 countErr=$(grep -iF  "socket_writer" /var/opt/microsoft/docker-cimprov/log/telegraf.log | wc -l | tr -d '\n')
-echo "telegraf,AKS_RESOURCE_ID=${AKS_RESOURCE_ID} telegrafTCPWriteErrorCountTotal=${countErr}i"
+echo "telegraf,Source=telegrafErrLog telegrafTCPWriteErrorCountTotal=${countErr}i"
