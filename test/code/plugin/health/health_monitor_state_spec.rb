2e50407703aafdb01572a41a4a2021e9862f3c83
r-dilip <dilip.rangarajan@gmail.com>
2019-07-17 11:19:29 -0700

    added integration tests for aks and aks-engine

--- test/code/plugin/health/health_monitor_state_spec.rb
+++ test/code/plugin/health/health_monitor_state_spec.rb
@@ -125,5 +125,52 @@ describe "HealthMonitorState spec" do
         monitor_state.new_state.must_equal "fail"
     end
 
+    it 'updates should_send to false for monitors which need consistent state change and has NO state change' do
+        #arrange
+        state = HealthMonitorState.new
+        mock_monitor = Mock.new
+        def mock_monitor.state; "pass"; end
+        def mock_monitor.monitor_id; "monitor_id"; end
+        def mock_monitor.monitor_instance_id; "monitor_instance_id"; end
+        def mock_monitor.transition_date_time; Time.now.utc.iso8601; end
+        def mock_monitor.details; {"state" => "pass", "timestamp" => Time.now.utc.iso8601, "details" => {}}; end
+
+        config = JSON.parse('{
+            "WarnThresholdPercentage": 80.0,
+            "FailThresholdPercentage": 90.0,
+            "ConsecutiveSamplesForStateTransition": 3
+        }')
+        #act
+        state.update_state(mock_monitor, config)
+        monitor_state = state.get_state("monitor_instance_id")
+        #assert
+        monitor_state.should_send.must_equal true
+        monitor_state.old_state.must_equal "none"
+        monitor_state.new_state.must_equal "none"
+
+
+        #arrange
+        def mock_monitor.state; "pass"; end
+        def mock_monitor.details; {"state" => "pass", "timestamp" => Time.now.utc.iso8601, "details" => {}}; end
+        #act
+        state.update_state(mock_monitor, config)
+        monitor_state = state.get_state("monitor_instance_id")
+        #assert
+        monitor_state.should_send.must_equal false
+
+        #act
+        state.update_state(mock_monitor, config)
+        monitor_state.should_send.must_equal true
+        monitor_state.old_state.must_equal "none"
+        monitor_state.new_state.must_equal "pass"
+
+        #act
+        state.update_state(mock_monitor, config)
+        monitor_state = state.get_state("monitor_instance_id")
+        #assert
+        monitor_state.should_send.must_equal false
+        monitor_state.old_state.must_equal "none"
+        monitor_state.new_state.must_equal "pass"
+    end
 
 end
