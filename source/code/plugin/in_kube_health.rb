4dce1dea64bd0f36e643cc58557a92d6f8c221fb
r-dilip <dilip.rangarajan@gmail.com>
2019-10-29 12:03:39 -0700

    Remove calls to build pod uid lookup

--- source/code/plugin/in_kube_health.rb
+++ source/code/plugin/in_kube_health.rb
@@ -91,7 +91,7 @@ module Fluent
         @resources.node_inventory = node_inventory
         @resources.pod_inventory = pod_inventory
         @resources.set_deployment_inventory(deployment_inventory)
-        @resources.build_pod_uid_lookup
+        #@resources.build_pod_uid_lookup
 
         if node_inventory_response.code.to_i != 200
           record = process_kube_api_up_monitor("fail", node_inventory_response)
@@ -296,7 +296,7 @@ module Fluent
         @resources.node_inventory = node_inventory
         @resources.pod_inventory = pod_inventory
         @resources.set_deployment_inventory(deployment_inventory)
-        @resources.build_pod_uid_lookup
+        #@resources.build_pod_uid_lookup
     end
 
     def run_periodic
