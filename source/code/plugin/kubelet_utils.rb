93568434f0a722d330f5656c76a60114e8a9bae6
Ganga Mahesh Siddem <gangams@microsoft.com>
2020-02-13 12:41:29 -0800

    wip

--- source/code/plugin/kubelet_utils.rb
+++ source/code/plugin/kubelet_utils.rb
@@ -26,25 +26,32 @@ class KubeletUtils
                 response = CAdvisorMetricsAPIClient.getPodsFromCAdvisor(winNode: nil)
                 if !response.nil? && !response.body.nil?
                     podList = JSON.parse(response.body)
-                    if !podList.nil? && !podList.empty? 
+                    if !podList.nil? && !podList.empty? && podList.key?("items") && !podList["items"].nil? && !podList["items"].empty?
                         podList["items"].each do |item|
-                            containersInfoMap = getContainersInfoMap(spec, clusterCollectEnvironmentVar)
-                            containerInstance = {}                                                                                          
+                            containersInfoMap = getContainersInfoMap(item, clusterCollectEnvironmentVar)
+                            containerInventoryRecord = {}                                                                                          
                             if !item["status"].nil? && !item["status"].empty?                              
                                 if !item["status"]["containerStatuses"].nil? && !item["status"]["containerStatuses"].empty?
                                     item["status"]["containerStatuses"].each do |containerStatus| 
-                                     containerInstance["CollectionTime"] = batchTime #This is the time that is mapped to become TimeGenerated    
+                                     containerInventoryRecord["CollectionTime"] = batchTime #This is the time that is mapped to become TimeGenerated    
                                      containerName = containerStatus["name"]    
-                                     containerInstance["ContainerID"] = containerStatus["containerID"].split('//')[0]
+                                     containerInventoryRecord["ContainerID"] = containerStatus["containerID"].split('//')[1]
+                                     repoImageTagArray = containerStatus["image"].split("/")
+                                     containerInventoryRecord["Repository"] = repoImageTagArray[0]
+                                     imageTagArray = repoImageTagArray[1].split(":")
+                                     containerInventoryRecord["Image"] = imageTagArray[0]
+                                     containerInventoryRecord["ImageTag"] = imageTagArray[1]
+                                     # Setting the image id to the id in the remote repository
+                                     containerInventoryRecord["ImageId"] = containerStatus["imageID"].split("@")[1]
                                      containerInfoMap = containersInfoMap[containerName]  
-                                     containerInstance["ElementName"] = containerInfoMap["ElementName"]
-                                     containerInstance["Computer"] = containerInfoMap["Computer"]
-                                     containerInstance["ContainerHostname"] = containerInfoMap["ContainerHostname"]
-                                     containerInstance["CreatedTime"] = containerInfoMap["CreatedTime"]
-                                     containerInstance["EnvironmentVar"] = containerInfoMap["EnvironmentVar"]
-                                     containerInstance["Ports"] = containerInfoMap["Ports"]
-                                     containerInstance["Command"] = containerInfoMap["Command"]
-                                     containerInventoryRecords.push containerInstance
+                                     containerInventoryRecord["ElementName"] = containerInfoMap["ElementName"]
+                                     containerInventoryRecord["Computer"] = containerInfoMap["Computer"]
+                                     containerInventoryRecord["ContainerHostname"] = containerInfoMap["ContainerHostname"]
+                                     containerInventoryRecord["CreatedTime"] = containerInfoMap["CreatedTime"]
+                                     containerInventoryRecord["EnvironmentVar"] = containerInfoMap["EnvironmentVar"]
+                                     containerInventoryRecord["Ports"] = containerInfoMap["Ports"]
+                                     containerInventoryRecord["Command"] = containerInfoMap["Command"]
+                                     containerInventoryRecords.push containerInventoryRecord
                                     end
                                 end 
                             end                                                      
@@ -60,26 +67,51 @@ class KubeletUtils
         def getContainersInfoMap(item, clusterCollectEnvironmentVar)
             containersInfoMap = {}
             begin
-                createdTime = item["metadata"]["creationTimestamp"]                  
                 nodeName = (!item["spec"]["nodeName"].nil?) ? item["spec"]["nodeName"] : ""                
-                item["spec"]["containers"].each do |container|
-                    containerInfoMap = {}
-                    containerName = container["name"]    
-                    containerInfoMap["ElementName"] = containerName
-                    containerInfoMap["Computer"] = nodeName             
-                    containerInfoMap["ContainerHostname"] = nodeName
-                    containerInfoMap["CreatedTime"] = createdTime
-                    envValue = container["env"]
-                    envValueString = (envValue.nil?) ? "" : envValue.to_s
-                    containerInfoMap["EnvironmentVar"] = envValueString  
-                    portsValue = container["ports"]
-                    portsValueString = (portsValue.nil?) ? "" : portsValue.to_s     
-                    containerInfoMap["Ports"] = portsValueString    
-                    argsValue = container["args"]
-                    argsValueString = (argsValue.nil?) ? "" : argsValue.to_s     
-                    containerInfoMap["Command"] = argsValueString
+                createdTime = item["metadata"]["creationTimestamp"]    
+                if !item.nil? && !item.empty? && item.key?("spec") && !item["spec"].nil? && !item["spec"].empty?   
+                    if !item["spec"]["containers"].nil? && !item["spec"]["containers"].empty?
+                        item["spec"]["containers"].each do |container|
+                            containerInfoMap = {}
+                            containerName = container["name"]    
+                            containerInfoMap["ElementName"] = containerName
+                            containerInfoMap["Computer"] = nodeName             
+                            containerInfoMap["ContainerHostname"] = nodeName
+                            containerInfoMap["CreatedTime"] = createdTime
 
-                    containersInfoMap[containerName] = containerInfoMap         
+                            if !clusterCollectEnvironmentVar.nil? && !clusterCollectEnvironmentVar.empty? && clusterCollectEnvironmentVar.casecmp("false") == 0
+                                containerInfoMap["EnvironmentVar"] = ["AZMON_CLUSTER_COLLECT_ENV_VAR=FALSE"]
+                            else
+                                envValue = container["env"]
+                                envValueString = (envValue.nil?) ? "" : envValue.to_s
+                                # Skip environment variable processing if it contains the flag AZMON_COLLECT_ENV=FALSE
+                                # Check to see if the environment variable collection is disabled for this container.
+                                if /AZMON_COLLECT_ENV=FALSE/i.match(envValueString)
+                                envValueString = ["AZMON_COLLECT_ENV=FALSE"]
+                                $log.warn("Environment Variable collection for container: #{containerName} skipped because AZMON_COLLECT_ENV is set to false")
+                                end
+                                # Restricting the ENV string value to 200kb since the size of this string can go very high
+                                if envValueString.length > 200000
+                                    envValueStringTruncated = envValueString.slice(0..200000)
+                                    lastIndex = envValueStringTruncated.rindex("\", ")
+                                    if !lastIndex.nil?
+                                        envValueStringTruncated = envValueStringTruncated.slice(0..lastIndex) + "]"
+                                    end
+                                    containerInfoMap["EnvironmentVar"] = envValueStringTruncated
+                                else
+                                    containerInfoMap["EnvironmentVar"] = envValueString
+                                end
+                            end              
+                            portsValue = container["ports"]
+                            portsValueString = (portsValue.nil?) ? "" : portsValue.to_s     
+                            containerInfoMap["Ports"] = portsValueString    
+                            argsValue = container["args"]
+                            argsValueString = (argsValue.nil?) ? "" : argsValue.to_s     
+                            containerInfoMap["Command"] = argsValueString
+
+                            containersInfoMap[containerName] = containerInfoMap         
+                        end    
+                    end
                 end                 
             rescue => error      
                 @Log.warn("kubelet_utils::getContainersInfoMap : Get Container Info Maps failed: #{error}")          
