d647a416747c1722b860cc523134ba02d5a306ef
Ganga Mahesh Siddem <gangams@microsoft.com>
2020-02-13 01:27:18 -0800

    wip

--- source/code/plugin/kubelet_utils.rb
+++ source/code/plugin/kubelet_utils.rb
@@ -21,34 +21,70 @@ class KubeletUtils
         end
 
         def getContainerInventoryRecords(batchTime, clusterCollectEnvironmentVar)    
-            containerInventoryRecords = Array.new        
-            response = CAdvisorMetricsAPIClient.getPodsFromCAdvisor(winNode: nil)
-            if !response.nil? && !response.body.nil?
-                podList = JSON.parse(response.body)
-                if !podList.nil? && !podList.empty? 
-                    podList["items"].each do |item| 
-                        nodeName = item["spec"]["nodeName"]
-                        containerInstance = {}                      
-                        status = item.status                        
-                        if !status.nil? && !status.empty?
-                            containerStatuses = status.containerStatuses
-                            if !containerStatuses.nil? && !containerStatuses.empty?
-                               containerStatuses.each do |item| 
-                                 containerInstance["ElementName"] = containerStatuses["name"]
-                                 containerInstance["InstanceID"] = container["containerID"]
-                                 containerInstance["Computer"] = nodeName
-                                 containerInstance["CollectionTime"] = batchTime #This is the time that is mapped to become TimeGenerated
-                               end
-                            end 
-                        end   
-                        containerSpec = item.spec
-                        containerInventoryRecords.push containerInstance
-                    end    
+            containerInventoryRecords = Array.new                   
+            begin
+                response = CAdvisorMetricsAPIClient.getPodsFromCAdvisor(winNode: nil)
+                if !response.nil? && !response.body.nil?
+                    podList = JSON.parse(response.body)
+                    if !podList.nil? && !podList.empty? 
+                        podList["items"].each do |item|
+                            containersInfoMap = getContainersInfoMap(spec, clusterCollectEnvironmentVar)
+                            containerInstance = {}                                                                                          
+                            if !item["status"].nil? && !item["status"].empty?                              
+                                if !item["status"]["containerStatuses"].nil? && !item["status"]["containerStatuses"].empty?
+                                    item["status"]["containerStatuses"].each do |containerStatus| 
+                                     containerInstance["CollectionTime"] = batchTime #This is the time that is mapped to become TimeGenerated    
+                                     containerName = containerStatus["name"]    
+                                     containerInstance["ContainerID"] = containerStatus["containerID"].split('//')[0]
+                                     containerInfoMap = containersInfoMap[containerName]  
+                                     containerInstance["ElementName"] = containerInfoMap["ElementName"]
+                                     containerInstance["Computer"] = containerInfoMap["Computer"]
+                                     containerInstance["ContainerHostname"] = containerInfoMap["ContainerHostname"]
+                                     containerInstance["CreatedTime"] = containerInfoMap["CreatedTime"]
+                                     containerInstance["EnvironmentVar"] = containerInfoMap["EnvironmentVar"]
+                                     containerInstance["Ports"] = containerInfoMap["Ports"]
+                                     containerInstance["Command"] = containerInfoMap["Command"]
+                                     containerInventoryRecords.push containerInstance
+                                    end
+                                end 
+                            end                                                      
+                        end    
+                    end
                 end
-            end
-
+            rescue => error
+                @Log.warn("kubelet_utils::getContainerInventoryRecords : Get Container Inventory Records failed: #{error}")
+            end          
             return containerInventoryRecords
         end
 
+        def getContainersInfoMap(item, clusterCollectEnvironmentVar)
+            containersInfoMap = {}
+            begin
+                createdTime = item["metadata"]["creationTimestamp"]                  
+                nodeName = (!item["spec"]["nodeName"].nil?) ? item["spec"]["nodeName"] : ""                
+                item["spec"]["containers"].each do |container|
+                    containerInfoMap = {}
+                    containerName = container["name"]    
+                    containerInfoMap["ElementName"] = containerName
+                    containerInfoMap["Computer"] = nodeName             
+                    containerInfoMap["ContainerHostname"] = nodeName
+                    containerInfoMap["CreatedTime"] = createdTime
+                    envValue = container["env"]
+                    envValueString = (envValue.nil?) ? "" : envValue.to_s
+                    containerInfoMap["EnvironmentVar"] = envValueString  
+                    portsValue = container["ports"]
+                    portsValueString = (portsValue.nil?) ? "" : portsValue.to_s     
+                    containerInfoMap["Ports"] = portsValueString    
+                    argsValue = container["args"]
+                    argsValueString = (argsValue.nil?) ? "" : argsValue.to_s     
+                    containerInfoMap["Command"] = argsValueString
+
+                    containersInfoMap[containerName] = containerInfoMap         
+                end                 
+            rescue => error      
+                @Log.warn("kubelet_utils::getContainersInfoMap : Get Container Info Maps failed: #{error}")          
+            end
+            return containersInfoMap
+        end  
     end
 end
