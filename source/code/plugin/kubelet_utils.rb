03796ae17669b1ad90ae96ea3cf118dcd551fa4e
Ganga Mahesh Siddem <gangams@microsoft.com>
2020-02-24 13:23:56 -0800

    add secretkeyref support

--- source/code/plugin/kubelet_utils.rb
+++ source/code/plugin/kubelet_utils.rb
@@ -193,7 +193,7 @@ class KubeletUtils
               elsif valueFrom.key?("secretKeyRef")
                 secretName = valueFrom["secretKeyRef"]["name"]
                 secretKey = valueFrom["secretKeyRef"]["key"]
-                # flatten value so that Ux can show that
+                # This is just secret not the plaintext. Flatten value so that Ux can show that
                 if !secretName.nil && !secretName.empty && !secretKey.nil? && !secretKey.empty?
                    value = "secretKeyRef_#{secretName}_#{secretKey}"
                 end
