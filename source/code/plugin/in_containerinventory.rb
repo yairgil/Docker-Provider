46ce17464b997ed7a6284ff6e1a62497d233560f
Ganga Mahesh Siddem <gangams@microsoft.com>
2020-03-16 12:37:08 -0700

    turnoff docker container inventory

--- source/code/plugin/in_containerinventory.rb
+++ source/code/plugin/in_containerinventory.rb
@@ -5,11 +5,7 @@ module Fluent
   class Container_Inventory_Input < Input
     Plugin.register_input("containerinventory", self)
 
-    @@PluginName = "ContainerInventory"
-    @@RunningState = "Running"
-    @@FailedState = "Failed"
-    @@StoppedState = "Stopped"
-    @@PausedState = "Paused"
+    @@PluginName = "ContainerInventory"   
 
     def initialize
       super
@@ -48,150 +44,8 @@ module Fluent
         }
         @thread.join
       end
-    end
-
-    def obtainContainerConfig(instance, container, clusterCollectEnvironmentVar)
-      begin
-        configValue = container["Config"]
-        if !configValue.nil?
-          instance["ContainerHostname"] = configValue["Hostname"]
-          # Check to see if the environment variable collection is disabled at the cluster level - This disables env variable collection for all containers.
-          if !clusterCollectEnvironmentVar.nil? && !clusterCollectEnvironmentVar.empty? && clusterCollectEnvironmentVar.casecmp("false") == 0
-            instance["EnvironmentVar"] = ["AZMON_CLUSTER_COLLECT_ENV_VAR=FALSE"]
-          else
-            envValue = configValue["Env"]
-            envValueString = (envValue.nil?) ? "" : envValue.to_s
-            # Skip environment variable processing if it contains the flag AZMON_COLLECT_ENV=FALSE
-            # Check to see if the environment variable collection is disabled for this container.
-            if /AZMON_COLLECT_ENV=FALSE/i.match(envValueString)
-              envValueString = ["AZMON_COLLECT_ENV=FALSE"]
-              $log.warn("Environment Variable collection for container: #{container["Id"]} skipped because AZMON_COLLECT_ENV is set to false")
-            end
-            # Restricting the ENV string value to 200kb since the size of this string can go very high
-            if envValueString.length > 200000
-              envValueStringTruncated = envValueString.slice(0..200000)
-              lastIndex = envValueStringTruncated.rindex("\", ")
-              if !lastIndex.nil?
-                envValueStringTruncated = envValueStringTruncated.slice(0..lastIndex) + "]"
-              end
-              instance["EnvironmentVar"] = envValueStringTruncated
-            else
-              instance["EnvironmentVar"] = envValueString
-            end
-          end
-
-          cmdValue = configValue["Cmd"]
-          cmdValueString = (cmdValue.nil?) ? "" : cmdValue.to_s
-          instance["Command"] = cmdValueString
-
-          instance["ComposeGroup"] = ""
-          labelsValue = configValue["Labels"]
-          if !labelsValue.nil? && !labelsValue.empty?
-            instance["ComposeGroup"] = labelsValue["com.docker.compose.project"]
-          end
-        else
-          $log.warn("Attempt in ObtainContainerConfig to get container: #{container["Id"]} config information returned null")
-        end
-      rescue => errorStr
-        $log.warn("Exception in obtainContainerConfig: #{errorStr}")
-      end
-    end
-
-    def obtainContainerState(instance, container)
-      begin
-        stateValue = container["State"]
-        if !stateValue.nil?
-          exitCodeValue = stateValue["ExitCode"]
-          # Exit codes less than 0 are not supported by the engine
-          if exitCodeValue < 0
-            exitCodeValue = 128
-            $log.info("obtainContainerState::Container: #{container["Id"]} returned negative exit code")
-          end
-          instance["ExitCode"] = exitCodeValue
-          if exitCodeValue > 0
-            instance["State"] = @@FailedState
-          else
-            # Set the Container status : Running/Paused/Stopped
-            runningValue = stateValue["Running"]
-            if runningValue
-              pausedValue = stateValue["Paused"]
-              # Checking for paused within running is true state because docker returns true for both Running and Paused fields when the container is paused
-              if pausedValue
-                instance["State"] = @@PausedState
-              else
-                instance["State"] = @@RunningState
-              end
-            else
-              instance["State"] = @@StoppedState
-            end
-          end
-          instance["StartedTime"] = stateValue["StartedAt"]
-          instance["FinishedTime"] = stateValue["FinishedAt"]
-        else
-          $log.info("Attempt in ObtainContainerState to get container: #{container["Id"]} state information returned null")
-        end
-      rescue => errorStr
-        $log.warn("Exception in obtainContainerState: #{errorStr}")
-      end
-    end
-
-    def obtainContainerHostConfig(instance, container)
-      begin
-        hostConfig = container["HostConfig"]
-        if !hostConfig.nil?
-          links = hostConfig["Links"]
-          instance["Links"] = ""
-          if !links.nil?
-            linksString = links.to_s
-            instance["Links"] = (linksString == "null") ? "" : linksString
-          end
-          portBindings = hostConfig["PortBindings"]
-          instance["Ports"] = ""
-          if !portBindings.nil?
-            portBindingsString = portBindings.to_s
-            instance["Ports"] = (portBindingsString == "null") ? "" : portBindingsString
-          end
-        else
-          $log.info("Attempt in ObtainContainerHostConfig to get container: #{container["Id"]} host config information returned null")
-        end
-      rescue => errorStr
-        $log.warn("Exception in obtainContainerHostConfig: #{errorStr}")
-      end
-    end
-
-    def inspectContainer(id, nameMap, clusterCollectEnvironmentVar)
-      containerInstance = {}
-      begin
-        container = DockerApiClient.dockerInspectContainer(id)
-        if !container.nil? && !container.empty?
-          containerInstance["InstanceID"] = container["Id"]
-          containerInstance["CreatedTime"] = container["Created"]
-          containerName = container["Name"]
-          if !containerName.nil? && !containerName.empty?
-            # Remove the leading / from the name if it exists (this is an API issue)
-            containerInstance["ElementName"] = (containerName[0] == "/") ? containerName[1..-1] : containerName
-          end
-          imageValue = container["Image"]
-          if !imageValue.nil? && !imageValue.empty?
-            repoImageTagArray = nameMap[imageValue]
-            if nameMap.has_key? imageValue
-              containerInstance["Repository"] = repoImageTagArray[0]
-              containerInstance["Image"] = repoImageTagArray[1]
-              containerInstance["ImageTag"] = repoImageTagArray[2]
-              # Setting the image id to the id in the remote repository
-              containerInstance["ImageId"] = repoImageTagArray[3]
-            end
-          end
-          obtainContainerConfig(containerInstance, container, clusterCollectEnvironmentVar)
-          obtainContainerState(containerInstance, container)
-          obtainContainerHostConfig(containerInstance, container)
-        end
-      rescue => errorStr
-        $log.warn("Exception in inspectContainer: #{errorStr} for container: #{id}")
-      end
-      return containerInstance
-    end
-
+    end   
+  
     def enumerate
       currentTime = Time.now
       emitTime = currentTime.to_f
@@ -204,41 +58,10 @@ module Fluent
         containerRuntimeEnv = ENV["CONTAINER_RUNTIME"]
         $log.info("in_container_inventory::enumerate : container runtime : #{containerRuntimeEnv}")
         clusterCollectEnvironmentVar = ENV["AZMON_CLUSTER_COLLECT_ENV_VAR"]
-        if !containerRuntimeEnv.nil? && !containerRuntimeEnv.empty? && containerRuntimeEnv.casecmp("docker") == 0
-          $log.info("in_container_inventory::enumerate : using docker apis since container runtime is docker")
-          hostName = DockerApiClient.getDockerHostName
-          containerIds = DockerApiClient.listContainers
-          if !containerIds.nil? && !containerIds.empty?
-            nameMap = DockerApiClient.getImageIdMap
-            if !clusterCollectEnvironmentVar.nil? && !clusterCollectEnvironmentVar.empty? && clusterCollectEnvironmentVar.casecmp("false") == 0
-              $log.warn("Environment Variable collection disabled for cluster")
-            end
-            containerIds.each do |containerId|
-              inspectedContainer = {}
-              inspectedContainer = inspectContainer(containerId, nameMap, clusterCollectEnvironmentVar)
-              inspectedContainer["Computer"] = hostName
-              inspectedContainer["CollectionTime"] = batchTime #This is the time that is mapped to become TimeGenerated
-              containerInventory.push inspectedContainer
-              ContainerInventoryState.writeContainerState(inspectedContainer)
-            end
-            # Update the state for deleted containers
-            deletedContainers = ContainerInventoryState.getDeletedContainers(containerIds)
-            if !deletedContainers.nil? && !deletedContainers.empty?
-              deletedContainers.each do |deletedContainer|
-                container = ContainerInventoryState.readContainerState(deletedContainer)
-                if !container.nil?
-                  container.each { |k, v| container[k] = v }
-                  container["State"] = "Deleted"
-                  containerInventory.push container
-                end
-              end
-            end
-          end
-        else
-          $log.info("in_container_inventory::enumerate : using cadvisor apis since non docker container runtime : #{containerRuntimeEnv}")                    
-          containerIds = Array.new
-          response = CAdvisorMetricsAPIClient.getPodsFromCAdvisor(winNode: nil)
-          if !response.nil? && !response.body.nil?
+        $log.info("in_container_inventory::enumerate : using cadvisor apis")                    
+        containerIds = Array.new
+        response = CAdvisorMetricsAPIClient.getPodsFromCAdvisor(winNode: nil)
+        if !response.nil? && !response.body.nil?
             podList = JSON.parse(response.body)
             if !podList.nil? && !podList.empty? && podList.key?("items") && !podList["items"].nil? && !podList["items"].empty?
               podList["items"].each do |item|
@@ -253,22 +76,20 @@ module Fluent
                 end           
               end
             end  
-          end                          
-          # Update the state for deleted containers
-          deletedContainers = ContainerInventoryState.getDeletedContainers(containerIds)
-          if !deletedContainers.nil? && !deletedContainers.empty?
-            deletedContainers.each do |deletedContainer|
-              container = ContainerInventoryState.readContainerState(deletedContainer)
-              if !container.nil?
-                container.each { |k, v| container[k] = v }
-                container["State"] = "Deleted"
-                @@containerCGroupCache.delete(container["InstanceID"])
-                containerInventory.push container
-              end
-            end
-          end
-        end
-
+        end                          
+        # Update the state for deleted containers
+        deletedContainers = ContainerInventoryState.getDeletedContainers(containerIds)
+        if !deletedContainers.nil? && !deletedContainers.empty?
+          deletedContainers.each do |deletedContainer|
+            container = ContainerInventoryState.readContainerState(deletedContainer)
+            if !container.nil?
+              container.each { |k, v| container[k] = v }
+              container["State"] = "Deleted"
+              @@containerCGroupCache.delete(container["InstanceID"])
+              containerInventory.push container
+             end
+           end
+        end        
         containerInventory.each do |record|
           wrapper = {
             "DataType" => "CONTAINER_INVENTORY_BLOB",
