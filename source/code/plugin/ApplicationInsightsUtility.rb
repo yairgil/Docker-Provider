02fd8f599ca2e3a3bcaf20762dd4bcacd195de4a
rashmichandrashekar <rashmy@microsoft.com>
2020-05-22 16:42:36 -0700

    May release bug fixes (#355)
    * fix health exceptions
    
    * Adding Computer to app insights telemetry
    
    Co-authored-by: rashmy <rashmy@RASHMY-PC2>

--- source/code/plugin/ApplicationInsightsUtility.rb
+++ source/code/plugin/ApplicationInsightsUtility.rb
@@ -65,6 +65,7 @@ class ApplicationInsightsUtility
         @@CustomProperties["WorkspaceID"] = getWorkspaceId
         @@CustomProperties["AgentVersion"] = ENV[@@EnvAgentVersion]
         @@CustomProperties["ControllerType"] = ENV[@@EnvControllerType]
+        @@CustomProperties["Computer"] = @@hostName
         encodedAppInsightsKey = ENV[@@EnvApplicationInsightsKey]
         appInsightsEndpoint = ENV[@@EnvApplicationInsightsEndpoint]
         @@CustomProperties["WorkspaceCloud"] = getWorkspaceCloud
