36226a06fe607a43cf2e2f709856140e1222d975
Ganga Mahesh Siddem <gangams@microsoft.com>
2020-06-18 20:02:21 -0700

    fix file permission for go

--- .pipelines/build.sh
+++ .pipelines/build.sh
@@ -5,6 +5,9 @@ set +e
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 echo $DIR
 
+# grant file permission for go bin
+sudo chmod 777 /usr/local/go/bin
+
 cd $DIR/../build/linux
 
 echo ----------- Build Docker Provider -------------------------------
