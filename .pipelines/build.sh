6770e9249f9e11bdd4ac2a15ce50e1522098de96
Ganga Mahesh Siddem <gangams@microsoft.com>
2020-06-18 20:12:38 -0700

    file permission

--- .pipelines/build.sh
+++ .pipelines/build.sh
@@ -6,7 +6,9 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 echo $DIR
 
 # grant file permission for go bin
+echo "start: grant file permissions for go bin"
 sudo chmod 777 /usr/local/go/bin
+echo "end: grant file permissions for go bin"
 
 cd $DIR/../build/linux
 
