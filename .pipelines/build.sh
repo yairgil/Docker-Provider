49f9d1ebf696244e93ab9e0067276cdbcd98a888
Ganga Mahesh Siddem <gangams@microsoft.com>
2020-06-19 15:39:21 -0700

    use latest app container image

--- .pipelines/build.sh
+++ .pipelines/build.sh
@@ -5,11 +5,6 @@ set +e
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 echo $DIR
 
-# grant file permission for go bin
-echo "start: grant file permissions for go bin"
-chmod -R 777 /usr/local/go
-echo "end: grant file permissions for go bin"
-
 cd $DIR/../build/linux
 
 echo ----------- Build Docker Provider -------------------------------
