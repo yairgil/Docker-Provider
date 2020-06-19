e5b0a2294e50c52306fb4bc418775359bf76afca
Ganga Mahesh Siddem <gangams@microsoft.com>
2020-06-19 09:03:47 -0700

    set rwe permission recursively

--- .pipelines/build.sh
+++ .pipelines/build.sh
@@ -7,7 +7,7 @@ echo $DIR
 
 # grant file permission for go bin
 echo "start: grant file permissions for go bin"
-chmod 777 /usr/local/go/bin
+chmod -R 777 /usr/local/go
 echo "end: grant file permissions for go bin"
 
 cd $DIR/../build/linux
