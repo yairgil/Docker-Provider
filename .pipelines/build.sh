0f75f6018ec03828188be0fd542319c97217aa82
Ganga Mahesh Siddem <gangams@microsoft.com>
2020-06-20 11:12:53 -0700

    update the build script

--- .pipelines/build.sh
+++ .pipelines/build.sh
@@ -5,6 +5,13 @@ set +e
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 echo $DIR
 
+echo "set GOPATH, GOARCH and GOOS env variables"
+export GOOS="linux"
+export GOARCH="amd64"
+export GOPATH=$DIR/../source/plugins/go
+echo "GOPATH:"$GOPATH
+echo "GOPROXY":$GOPROXY
+go env
 echo "start:get the go modules"
 cd $DIR/../source/plugins/go/src
 go get
