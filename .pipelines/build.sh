7f7958d848a65fcda637be2fcfb9092d7c6171a5
Ganga Mahesh Siddem <gangams@microsoft.com>
2020-06-19 18:40:40 -0700

    update build script

--- .pipelines/build.sh
+++ .pipelines/build.sh
@@ -5,10 +5,12 @@ set +e
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 echo $DIR
 
-cd $DIR/../build/linux
-
-echo "get the go modules"
+echo "start:get the go modules"
+cd $DIR/../source/plugins/go/src
 go get
+echo "end:get the go modules"
+
+cd $DIR/../build/linux
 echo "----------- Build Docker Provider -------------------------------"
 make
 cd $DIR
