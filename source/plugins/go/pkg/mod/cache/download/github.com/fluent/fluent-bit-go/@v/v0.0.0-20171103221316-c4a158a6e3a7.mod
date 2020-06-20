3db5d8a182ef10622ac10529f802a33c0ac229c8
Ganga Mahesh Siddem <gangams@microsoft.com>
2020-06-20 12:49:20 -0700

    add packages as workaround

---
+++ source/plugins/go/pkg/mod/cache/download/github.com/fluent/fluent-bit-go/@v/v0.0.0-20171103221316-c4a158a6e3a7.mod
@@ -0,0 +1,1 @@
+module github.com/fluent/fluent-bit-go
---
+++ source/plugins/go/pkg/mod/github.com/microsoft/!application!insights-!go@v0.4.3/appinsights/configuration_test.go
@@ -0,0 +1,22 @@
+package appinsights
+
+import "testing"
+
+func TestTelemetryConfiguration(t *testing.T) {
+	testKey := "test"
+	defaultEndpoint := "https://dc.services.visualstudio.com/v2/track"
+
+	config := NewTelemetryConfiguration(testKey)
+
+	if config.InstrumentationKey != testKey {
+		t.Errorf("InstrumentationKey is %s, want %s", config.InstrumentationKey, testKey)
+	}
+
+	if config.EndpointUrl != defaultEndpoint {
+		t.Errorf("EndpointUrl is %s, want %s", config.EndpointUrl, defaultEndpoint)
+	}
+
+	if config.Client != nil {
+		t.Errorf("Client is not nil, want nil")
+	}
+}
