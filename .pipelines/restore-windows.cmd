55c2c841a133846c4e0713e0484634305eb623af
Ganga Mahesh Siddem <gangams@microsoft.com>
2020-06-22 21:16:35 -0700

    add publish command back to restore

--- .pipelines/restore-windows.cmd
+++ .pipelines/restore-windows.cmd
@@ -3,6 +3,7 @@ cd /D "%~dp0..\build\windows\installer\certificategenerator"
 dotnet add package Newtonsoft.json
 dotnet add package BouncyCastle
 dotnet restore CertificateGenerator.csproj
+dotnet publish -c Release -r win10-x64
 echo "COMPLETE:Adding and restoring dotnet packages"
 
 echo "START:Getting go packages"
