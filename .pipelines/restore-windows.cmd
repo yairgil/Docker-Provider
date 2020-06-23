7f7edae6149b9f12a409df1869a132229619e31d
Ganga Mahesh Siddem <gangams@microsoft.com>
2020-06-22 22:51:03 -0700

    add missing envvars in restore

--- .pipelines/restore-windows.cmd
+++ .pipelines/restore-windows.cmd
@@ -4,9 +4,14 @@ dotnet add package Newtonsoft.json
 dotnet add package BouncyCastle
 dotnet restore CertificateGenerator.csproj
 dotnet publish -c Release -r win10-x64
-echo "COMPLETE:Adding and restoring dotnet packages"
+echo "END:Adding and restoring dotnet packages"
+
+echo "START:set environment variables"
+setx "%path%;c:\gcc\bin"
+setx path "%path%;c:\gcc\bin;c:\go\bin"
+echo "END:set environment variables"
 
 echo "START:Getting go packages"
 cd /D "%~dp0..\source\plugins\go\src"
 c:\go\bin\go.exe get
-echo "COMPLETE:Getting go packages"
+echo "END:Getting go packages"
