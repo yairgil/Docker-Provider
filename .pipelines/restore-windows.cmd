7f611e9d7f8dcd1831d28b4395f45e156abb39e0
Ganga Mahesh Siddem <gangams@microsoft.com>
2020-06-22 20:22:29 -0700

    add publish command to restore

--- .pipelines/restore-windows.cmd
+++ .pipelines/restore-windows.cmd
@@ -1,4 +1,5 @@
 cd /D "%~dp0..\build\windows\installer\certificategenerator"
 dotnet add package Newtonsoft.json
 dotnet add package BouncyCastle
+dotnet publish -c Release -r win10-x64
 dotnet restore CertificateGenerator.csproj || exit /b 1
