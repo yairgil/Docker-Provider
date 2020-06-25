b158027cf3d4ef876f4395e55278fadc5c5ffd27
Dilip Raghunathan <dilip.rangarajan@gmail.com>
2020-06-24 18:28:20 -0700

    File System watcher changes for ADX Secret (#372)
    * Working changes for adx
    
    * watch ADX folder for adx secret for windows
    
    * Revert Dockerfile changes
    
    Co-authored-by: Dilip Raghunathan <dilipr@microsoft.com>

--- build/windows/installer/scripts/filesystemwatcher.ps1
+++ build/windows/installer/scripts/filesystemwatcher.ps1
@@ -1,39 +1,39 @@
-
 Start-Transcript -Path fileSystemWatcherTranscript.txt
 Write-Host "Removing Existing Event Subscribers"
 Get-EventSubscriber -Force | ForEach-Object { $_.SubscriptionId } | ForEach-Object { Unregister-Event -SubscriptionId $_ }
 Write-Host "Starting File System Watcher for config map updates"
-$FileSystemWatcher = New-Object System.IO.FileSystemWatcher
-$Path = "C:\etc\config\settings"
-$FileSystemWatcher.Path = $Path
-$FileSystemWatcher.IncludeSubdirectories = $True
-$EventName = 'Changed', 'Created', 'Deleted', 'Renamed'
-$user = [System.Security.Principal.WindowsIdentity]::GetCurrent().Name
-Write-Host $user
-Write-Host $env:USERPROFILE
 
-$Action = {
-    $fileSystemWatcherStatusPath = "C:\etc\omsagentwindows\filesystemwatcher.txt"
-    $fileSystemWatcherLog = "{0} was  {1} at {2}" -f $Event.SourceEventArgs.FullPath,
-    $Event.SourceEventArgs.ChangeType,
-    $Event.TimeGenerated
-    Write-Host $fileSystemWatcherLog
-    Add-Content -Path $fileSystemWatcherStatusPath -Value $fileSystemWatcherLog
+$Paths = @("C:\etc\config\settings", "C:\etc\config\adx")
+
+foreach ($path in $Paths)
+{
+    $FileSystemWatcher = New-Object System.IO.FileSystemWatcher
+    $FileSystemWatcher.Path = $path
+    $FileSystemWatcher.IncludeSubdirectories = $true
+    $EventName = 'Changed', 'Created', 'Deleted', 'Renamed'
+
+    $Action = {
+        $fileSystemWatcherStatusPath = "C:\etc\omsagentwindows\filesystemwatcher.txt"
+        $fileSystemWatcherLog = "{0} was  {1} at {2}" -f $Event.SourceEventArgs.FullPath,
+        $Event.SourceEventArgs.ChangeType,
+        $Event.TimeGenerated
+        Write-Host $fileSystemWatcherLog
+        Add-Content -Path $fileSystemWatcherStatusPath -Value $fileSystemWatcherLog
+    }
+
+    $ObjectEventParams = @{
+        InputObject = $FileSystemWatcher
+        Action      = $Action
+    }
+
+    ForEach ($Item in $EventName) {
+        $ObjectEventParams.EventName = $Item
+        $name = Split-Path -Path $path -Leaf
+        $ObjectEventParams.SourceIdentifier = "$($name).$($Item)"
+        Write-Host  "Starting watcher for Event: $($path).$($Item)"
+        $Null = Register-ObjectEvent  @ObjectEventParams
+    }
 }
-
-$ObjectEventParams = @{
-    InputObject = $FileSystemWatcher
-    Action      = $Action
-}
-
-ForEach ($Item in $EventName) {
-    $ObjectEventParams.EventName = $Item
-    $ObjectEventParams.SourceIdentifier = "File.$($Item)"
-    Write-Host  "Starting watcher for Event: $($Item)"
-    $Null = Register-ObjectEvent  @ObjectEventParams
-}
-
-Get-EventSubscriber -Force
 
 # keep this running for the container's lifetime, so that it can listen for changes to the config map mount path
 try
