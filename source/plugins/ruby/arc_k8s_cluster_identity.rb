fa971c5243388572617030c920878fe8aff7b26a
Ganga Mahesh Siddem <gangams@microsoft.com>
2020-07-30 00:41:37 -0700

    more refactoring

--- source/plugins/ruby/arc_k8s_cluster_identity.rb
+++ source/plugins/ruby/arc_k8s_cluster_identity.rb
@@ -9,26 +9,35 @@ require "time"
 
 class ArcK8sClusterIdentity
 
-  @@crd_resource_uri_template = "%{kube_api_server_url}/apis/clusterconfig.azure.com/v1beta1/namespaces/azure-arc/azureclusteridentityrequests/container-insights-clusteridentityrequest"
-  @@secret_resource_uri_template = "%{kube_api_server_url}/api/v1/namespaces/azure-arc/secrets/%{token_secret_name}"
+  @@cluster_config_crd_api_version = "clusterconfig.azure.com/v1beta1"
+  @@cluster_identity_resource_name = "container-insights-clusteridentityrequest"
+  @@cluster_identity_resource_namespace = "azure-arc"
+  @@cluster_identity_token_secret_namespace = "azure-arc"
+  @@crd_resource_uri_template = "%{kube_api_server_url}/apis/%{cluster_config_crd_api_version}/namespaces/%{cluster_identity_resource_namespace}/azureclusteridentityrequests/%{cluster_identity_resource_name}"
+  @@secret_resource_uri_template = "%{kube_api_server_url}/api/v1/namespaces/%{cluster_identity_token_secret_namespace}/secrets/%{token_secret_name}"
+  @@azure_monitor_custom_metrics_audience = "https://monitoring.azure.com/"
+  @@cluster_identity_request_kind = "AzureClusterIdentityRequest"
 
   def initialize
     @token_expiry_time = Time.now
     @cached_access_token = String.new
     @token_file_path = "/var/run/secrets/kubernetes.io/serviceaccount/token"
     @cert_file_path = "/var/run/secrets/kubernetes.io/serviceaccount/ca.crt"
+    @kube_api_server_url = get_kube_api_server_url
     @http_client = get_http_client
+    @service_account_token = get_service_account_token
   end
 
-  def get_token_from_secret(token_secret_name, token_secret_data_name, service_account_token)
+  def get_token_from_secret(token_secret_name, token_secret_data_name)
     token = nil
     begin
       secret_request_uri = @@secret_resource_uri_template % {
-        kube_api_server_url: kube_api_server_url,
+        kube_api_server_url: @kube_api_server_url,
+        cluster_identity_token_secret_namespace: @@cluster_identity_token_secret_namespace,
         token_secret_name: token_secret_name,
       }
       get_request = Net::HTTP::Get.new(secret_request_uri)
-      get_request["Authorization"] = "Bearer #{service_account_token}"
+      get_request["Authorization"] = "Bearer #{@service_account_token}"
       $log.info "Making GET request to #{secret_request_uri} @ #{Time.now.utc.iso8601}"
       get_response = @http_client.request(get_request)
       $log.info "Got response of #{get_response.code} for #{secret_request_uri} @ #{Time.now.utc.iso8601}"
@@ -43,15 +52,16 @@ class ArcK8sClusterIdentity
     return token
   end
 
-  def get_token_reference_from_crd(service_account_token)
+  def get_token_reference_from_crd()
     tokenReference = {}
     begin
-      kube_api_server_url = get_kube_api_server_url
       crd_request_uri = @@crd_resource_uri_template % {
-        kube_api_server_url: kube_api_server_url,
+        kube_api_server_url: @kube_api_server_url,
+        cluster_config_crd_api_version: @@cluster_config_crd_api_version,
+        cluster_identity_resource_namespace: @@cluster_identity_resource_namespace,
       }
       get_request = Net::HTTP::Get.new(crd_request_uri)
-      get_request["Authorization"] = "Bearer #{service_account_token}"
+      get_request["Authorization"] = "Bearer #{@service_account_token}"
       $log.info "Making GET request to #{crd_request_uri} @ #{Time.now.utc.iso8601}"
       get_response = @http_client.request(get_request)
       $log.info "Got response of #{get_response.code} for #{crd_request_uri} @ #{Time.now.utc.iso8601}"
@@ -67,14 +77,15 @@ class ArcK8sClusterIdentity
     return tokenReference
   end
 
-  def renew_near_expiry_token(service_account_token)
+  def renew_near_expiry_token()
     begin
-      kube_api_server_url = get_kube_api_server_url
       crd_request_uri = @@crd_resource_uri_template % {
-        kube_api_server_url: kube_api_server_url,
+        kube_api_server_url: @kube_api_server_url,
+        cluster_config_crd_api_version: @@cluster_config_crd_api_version,
+        cluster_identity_resource_namespace: @@cluster_identity_resource_namespace,
       }
       get_request = Net::HTTP::Get.new(crd_request_uri)
-      get_request["Authorization"] = "Bearer #{service_account_token}"
+      get_request["Authorization"] = "Bearer #{@service_account_token}"
       $log.info "Making GET request to #{crd_request_uri} @ #{Time.now.utc.iso8601}"
       get_response = @http_client.request(get_request)
       $log.info "Got response of #{get_response.code} for #{crd_request_uri} @ #{Time.now.utc.iso8601}"
@@ -87,7 +98,7 @@ class ArcK8sClusterIdentity
         update_request = Net::HTTP::Patch.new(crd_request_uri)
         update_request["Content-Type"] = "application/merge-patch+json"
       end
-      update_request["Authorization"] = "Bearer #{service_account_token}"
+      update_request["Authorization"] = "Bearer #{@service_account_token}"
       update_request_body = get_update_request_body
       update_request.body = update_request_body.to_json
       update_response = @http_client.request(update_request)
@@ -97,82 +108,35 @@ class ArcK8sClusterIdentity
     end
   end
 
-  def get_cluster_identity_token
+  def get_cluster_identity_token()
     begin
       # get the cluster msi identity token either if its empty or near expirty. Token is valid 24 hrs.
       if @cached_access_token.to_s.empty? || (Time.now + 60 * 60 > @token_expiry_time) # Refresh token 1 hr from expiration
-        service_account_token = get_service_account_token
-        # renew the token if its near expiry
-        if !@cached_access_token.to_s.empty? && (Time.now + 60 * 60 > @token_expiry_time)
-          renew_near_expiry_token(service_account_token)
-          # sleep 30 seconds to get the renewed token  available
-          sleep 30
-        end
-        tokneReference = get_token_reference_from_crd(service_account_token)
-        if !tokneReference.nil? && !tokneReference.empty?
-          @token_expiry_time = Time.parse(tokneReference["expirationTime"])
-          @token_secret_name = tokneReference["secretName"]
-          @token_secret_data_name = tokneReference["dataName"]
-          # get the token from secret
-          token = get_token_from_secret(@token_secret_name, @token_secret_data_name, service_account_token)
-          if !token.nil?
-            @cached_access_token = token
-          else
-            $log.warn ("got token nil from secret: #{@token_secret_name}")
+          # renew the token if its near expiry
+          if !@cached_access_token.to_s.empty? && (Time.now + 60 * 60 > @token_expiry_time)
+            renew_near_expiry_token
+            # sleep 30 seconds to get the renewed token  available
+            sleep 30
           end
-        end
+          tokneReference = get_token_reference_from_crd
+          if !tokneReference.nil? && !tokneReference.empty?
+            @token_expiry_time = Time.parse(tokneReference["expirationTime"])
+            token_secret_name = tokneReference["secretName"]
+            token_secret_data_name = tokneReference["dataName"]
+            # get the token from secret
+            token = get_token_from_secret(token_secret_name, token_secret_data_name)
+            if !token.nil?
+              @cached_access_token = token
+            else
+              $log.warn ("got token nil from secret: #{@token_secret_name}")
+            end
+          end
       end
     rescue => err
       $log.warn ("get_cluster_identity_token failed: #{err}")
     end
     return @cached_access_token
   end
-
-  # def get_cluster_identity_token
-  #   # get the cluster msi identity token which is valid for 24 hrs.
-  #   if @cached_access_token.to_s.empty? || (Time.now + 60 * 60 > @token_expiry_time) # Refresh token 1 hr from expiration
-  #     kube_api_server_url = get_kube_api_server_url
-  #     crd_request_uri = @@crd_resource_uri_template % {
-  #       kube_api_server_url: kube_api_server_url,
-  #     }
-  #     service_account_token = get_service_account_token
-  #     get_request = Net::HTTP::Get.new(crd_request_uri)
-  #     get_request["Authorization"] = "Bearer #{service_account_token}"
-  #     $log.info "Making GET request to #{crd_request_uri} @ #{Time.now.utc.iso8601}"
-  #     get_response = @http_client.request(get_request)
-  #     $log.info "Got response of #{get_response.code} for #{crd_request_uri} @ #{Time.now.utc.iso8601}"
-
-  #     if get_response.code.to_i == 200
-  #       status = JSON.parse(get_response.body)["status"]
-  #       tokenReference = status["tokenReference"]
-  #       @token_expiry_time = Time.parse(status["expirationTime"])
-  #       @token_secret_name = status["tokenReference"]["secretName"]
-  #       @token_secret_data_name = status["tokenReference"]["dataName"]
-  #     end
-
-  #     if !@token_secret_name.empty? && !@token_secret_name.empty?
-  #       # get the token from secret
-  #       secret_request_uri = @@secret_resource_uri_template % {
-  #         kube_api_server_url: kube_api_server_url,
-  #         token_secret_name: @token_secret_name,
-  #       }
-
-  #       get_request = Net::HTTP::Get.new(secret_request_uri)
-  #       get_request["Authorization"] = "Bearer #{service_account_token}"
-  #       $log.info "Making GET request to #{secret_request_uri} @ #{Time.now.utc.iso8601}"
-  #       get_response = @http_client.request(get_request)
-  #       $log.info "Got response of #{get_response.code} for #{secret_request_uri} @ #{Time.now.utc.iso8601}"
-  #       if get_response.code.to_i == 200
-  #         token_secret = JSON.parse(get_response.body)["data"]
-  #         cluster_identity_token = token_secret[@token_secret_data_name]
-  #         @cached_access_token = Base64.decode64(cluster_identity_token)
-  #       end
-  #     else
-  #       $log.warn ("Failed to get the cluster identity token secret")
-  #     end
-  #   end
-  #   return @cached_access_token
-  # end
 
   private
   def get_service_account_token()
@@ -212,13 +176,13 @@ class ArcK8sClusterIdentity
 
   def get_update_request_body
     body = {}
-    body["apiVersion"] = "clusterconfig.azure.com/v1beta1"
-    body["kind"] = "AzureClusterIdentityRequest"
+    body["apiVersion"] = @@cluster_config_crd_api_version
+    body["kind"] = @@cluster_identity_request_kind
     body["metadata"] = {}
-    body["metadata"]["name"] = "container-insights-clusteridentityrequest"
-    body["metadata"]["namespace"]  = "azure-arc"
+    body["metadata"]["name"] = @@cluster_identity_resource_name
+    body["metadata"]["namespace"]  = @@cluster_identity_resource_namespace
     body["spec"] = {}
-    body["spec"]["audience"] = "https://monitoring.azure.com/"
+    body["spec"]["audience"] = @@azure_monitor_custom_metrics_audience
     return body
   end
 
