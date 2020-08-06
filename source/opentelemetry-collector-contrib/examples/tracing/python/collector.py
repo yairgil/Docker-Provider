4f10dd07cfc42e12adbe71df4fd3541322d7e831
Ayushi Singh <ayushee.singh@gmail.com>
2020-08-06 12:33:10 -0700

    Final collector changes (#418)
    * python example changes
    
    * updated example
    
    * removed build files
    
    * changed otel-collector to omsagent-otel
    
    * changed service name
    
    * fixed service discovery
    
    * deleted job.yaml
    
    * added new line to eof
    
    * Delete tasks.json
    
    * Delete existingClusterOnboarding.json
    
    * Delete existingClusterParam.json
    
    * Delete Dockerfile
    
    * Delete scale
    
    * Delete telemetry-otel.go
    
    * Delete go.sum
    
    * Delete go.mod
    
    * Delete scale.py
    
    * Delete go.sum
    
    * Delete go.mod
    
    * Delete scale-collector.py
    
    * Delete main.go
    
    * Delete run.sh
    
    * Delete components.go
    
    * Update README.md
    
    * updated AI auth
    
    * reverting credentials to placeholder value
    
    * updated image from ACR

--- source/opentelemetry-collector-contrib/examples/tracing/python/collector.py
+++ source/opentelemetry-collector-contrib/examples/tracing/python/collector.py
@@ -1,6 +1,8 @@
 import flask
+import logging
 import requests
 import sys
+import threading
 
 from opentelemetry import trace
 from opentelemetry.ext.flask import FlaskInstrumentor
@@ -9,20 +11,16 @@ from opentelemetry.sdk.resources import Resource
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import BatchExportSpanProcessor
 from opentelemetry.ext.otlp.trace_exporter import OTLPSpanExporter
-# from opentelemetry.ext.opencensusexporter.trace_exporter import (
-#   OpenCensusSpanExporter,
-# )
 
-my_port = 5001
+my_port = 0
+if len(sys.argv) > 1:
+    my_port = int(sys.argv[1])
+else:
+    print("need to specify port number as first parameter")
 
-# exporter = OpenCensusSpanExporter(
-#     service_name="basic-service-%s" % my_port, endpoint="otel-collector:55678"
-# )
+exporter = OTLPSpanExporter(endpoint="omsagent-otel.kube-system:55680")
 
-exporter = OTLPSpanExporter(endpoint="otel-collector:55680")
-
-# trace.set_tracer_provider(TracerProvider())
-trace.set_tracer_provider(TracerProvider(resource=Resource(labels={ "service.name": "basic-service" })))
+trace.set_tracer_provider(TracerProvider(resource=Resource(labels={ "service.name": "basic-service-%s" % my_port})))
 span_processor = BatchExportSpanProcessor(exporter)
 trace.get_tracer_provider().add_span_processor(span_processor)
 
@@ -30,16 +28,21 @@ app = flask.Flask(__name__)
 FlaskInstrumentor().instrument_app(app)
 RequestsInstrumentor().instrument()
 
+forward_port = 0
+if len(sys.argv) > 2:
+    forward_port = int(sys.argv[2])
+
 @app.route("/")
 def pull_requests():
+    global forward_port
+
     # Fetch a list of pull requests on the opentracing repository
     github_url = "https://api.github.com/repos/opentracing/opentracing-python/pulls"
     r = requests.get(github_url)
 
-    json = r.json()
-    pull_request_titles = map(lambda item: item['title'], json)
+    if forward_port:
+        requests.get("http://localhost:%s" % forward_port)
 
-    return 'OpenTracing Pull Requests: ' + ', '.join(pull_request_titles)
+    return 'Success!'
 
-# App will run when http://localhost:5001/ is hit
 app.run(debug=True, port=my_port)
--- source/opentelemetry-collector-contrib/internal/common/go.mod
+++
@@ -1,5 +0,0 @@
-module github.com/open-telemetry/opentelemetry-collector-contrib/internal/common
-
-go 1.14
-
-require k8s.io/client-go v0.0.0-20190620085101-78d2af792bab
