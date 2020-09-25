94a1481da6dccd1f904b7c4d5a918bbe4ccda906
Grace Wehner <grace.wehner@microsoft.com>
2020-09-25 13:44:01 -0700

    collect volume name

--- source/plugins/ruby/CAdvisorMetricsAPIClient.rb
+++ source/plugins/ruby/CAdvisorMetricsAPIClient.rb
@@ -359,6 +359,7 @@ class CAdvisorMetricsAPIClient
                   metricTags[Constants::INSIGHTSMETRICS_TAGS_POD_NAME] = podName
                   metricTags[Constants::INSIGHTSMETRICS_TAGS_PVC_NAME] = pvcName
                   metricTags[Constants::INSIGHTSMETRICS_TAGS_PVC_NAMESPACE] = pvcNamespace
+                  metricTags[Constants::INSIGHTSMETRICS_TAGS_VOLUME_NAME] = volume["name"]
                   metricTags[Constants::INSIGHTSMETRICS_TAGS_PV_CAPACITY_BYTES] = volume["capacityBytes"]
 
                   metricItem["Tags"] = metricTags
