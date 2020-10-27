6a76451f54742fda24ec5e654d3179a204e04d85
vishwanath <visnara@microsoft.com>
2020-10-27 16:45:19 -0700

    Changes for cutting a new build for ciprod10272020 release

--- scripts/onboarding/managed/upgrade-monitoring.sh
+++ scripts/onboarding/managed/upgrade-monitoring.sh
@@ -20,7 +20,7 @@ set -e
 set -o pipefail
 
 # released chart version for Azure Arc enabled Kubernetes public preview
-mcrChartVersion="2.7.7"
+mcrChartVersion="2.7.8"
 mcr="mcr.microsoft.com"
 mcrChartRepoPath="azuremonitor/containerinsights/preview/azuremonitor-containers"
 
