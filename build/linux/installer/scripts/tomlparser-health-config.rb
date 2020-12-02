79f40f1bea726de0f5e337d98626da817c2e1d1f
Ganga Mahesh Siddem <gangams@microsoft.com>
2020-12-01 17:06:29 -0800

    configmap changes

--- build/linux/installer/scripts/tomlparser-health-config.rb
+++ build/linux/installer/scripts/tomlparser-health-config.rb
@@ -13,22 +13,31 @@ require_relative "ConfigParseErrorLogger"
 @configMapMountPath = "/etc/config/settings/agent-settings"
 @configSchemaVersion = ""
 @enable_health_model = false
+@nodesChunkSize = 0
+@podsChunkSize = 0
+@eventsChunkSize = 0
+@deploymentsChunkSize = 0
+@hpaChunkSize = 0
+
+def is_number?(value)
+  true if Integer(value) rescue false
+end
 
 # Use parser to parse the configmap toml file to a ruby structure
 def parseConfigMap
   begin
     # Check to see if config map is created
     if (File.file?(@configMapMountPath))
-      puts "config::configmap container-azm-ms-agentconfig for agent health settings mounted, parsing values"
+      puts "config::configmap container-azm-ms-agentconfig for agent settings mounted, parsing values"
       parsedConfig = Tomlrb.load_file(@configMapMountPath, symbolize_keys: true)
       puts "config::Successfully parsed mounted config map"
       return parsedConfig
     else
-      puts "config::configmap container-azm-ms-agentconfig for agent health settings not mounted, using defaults"
+      puts "config::configmap container-azm-ms-agentconfig for agent settings not mounted, using defaults"
       return nil
     end
   rescue => errorStr
-    ConfigParseErrorLogger.logError("Exception while parsing config map for enabling health: #{errorStr}, using defaults, please check config map for errors")
+    ConfigParseErrorLogger.logError("Exception while parsing config map for agent settings : #{errorStr}, using defaults, please check config map for errors")
     return nil
   end
 end
@@ -36,9 +45,39 @@ end
 # Use the ruby structure created after config parsing to set the right values to be used as environment variables
 def populateSettingValuesFromConfigMap(parsedConfig)
   begin
-    if !parsedConfig.nil? && !parsedConfig[:agent_settings].nil? && !parsedConfig[:agent_settings][:health_model].nil? && !parsedConfig[:agent_settings][:health_model][:enabled].nil?
+    if !parsedConfig.nil? && !parsedConfig[:agent_settings].nil?
+      if !parsedConfig[:agent_settings][:health_model].nil? && !parsedConfig[:agent_settings][:health_model][:enabled].nil?
         @enable_health_model = parsedConfig[:agent_settings][:health_model][:enabled]
         puts "enable_health_model = #{@enable_health_model}"
+      end
+      chunk_config = parsedConfig[:agent_settings][:chunk_config]
+      if !chunk_config.nil?
+        nodesChunkSize = chunk_config[:NODES_CHUNK_SIZE]
+        if !nodesChunkSize.nil? && is_number?(nodesChunkSize)
+          @nodesChunkSize = nodesChunkSize.to_i
+          puts "NODES_CHUNK_SIZE = #{@nodesChunkSize}"
+        end
+        podsChunkSize = chunk_config[:PODS_CHUNK_SIZE]
+        if !podsChunkSize.nil? && is_number?(podsChunkSize)
+          @podsChunkSize = podsChunkSize.to_i
+          puts "PODS_CHUNK_SIZE = #{@podsChunkSize}"
+        end
+        eventsChunkSize = chunk_config[:EVENTS_CHUNK_SIZE]
+        if !eventsChunkSize.nil? && is_number?(eventsChunkSize)
+          @eventsChunkSize = eventsChunkSize.to_i
+          puts "EVENTS_CHUNK_SIZE = #{@eventsChunkSize}"
+        end
+        deploymentsChunkSize = chunk_config[:DEPLOYMENTS_CHUNK_SIZE]
+        if !deploymentsChunkSize.nil? && is_number?(deploymentsChunkSize)
+          @deploymentsChunkSize = deploymentsChunkSize.to_i
+          puts "DEPLOYMENTS_CHUNK_SIZE = #{@deploymentsChunkSize}"
+        end
+        hpaChunkSize = chunk_config[:HPA_CHUNK_SIZE]
+        if !hpaChunkSize.nil? && is_number?(hpaChunkSize)
+          @hpaChunkSize = hpaChunkSize.to_i
+          puts "HPA_CHUNK_SIZE = #{@hpaChunkSize}"
+        end
+      end
     end
   rescue => errorStr
     puts "config::error:Exception while reading config settings for health_model enabled setting - #{errorStr}, using defaults"
@@ -65,6 +104,21 @@ file = File.open("health_config_env_var", "w")
 
 if !file.nil?
   file.write("export AZMON_CLUSTER_ENABLE_HEALTH_MODEL=#{@enable_health_model}\n")
+  if @nodesChunkSize > 0
+    file.write("export NODES_CHUNK_SIZE=#{@nodesChunkSize}\n")
+  end
+  if @podsChunkSize > 0
+    file.write("export PODS_CHUNK_SIZE=#{@podsChunkSize}\n")
+  end
+  if @eventsChunkSize > 0
+    file.write("export EVENTS_CHUNK_SIZE=#{@eventsChunkSize}\n")
+  end
+  if @deploymentsChunkSize > 0
+    file.write("export DEPLOYMENTS_CHUNK_SIZE=#{@deploymentsChunkSize}\n")
+  end
+  if @hpaChunkSize > 0
+    file.write("export HPA_CHUNK_SIZE=#{@hpaChunkSize}\n")
+  end
   # Close file after writing all environment variables
   file.close
 else
