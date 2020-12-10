475bc315d1e31370d4eed7e5a8b6748d10ff8ed9
galofir <galofir@microsoft.com>
2020-12-10 19:00:00 +0200

    script organization and remove redundant scope binding

--- ascdeployment/ServiceGroupRoot/Scripts/pushImageToAcr.sh
+++ ascdeployment/ServiceGroupRoot/Scripts/pushImageToAcr.sh
@@ -8,29 +8,28 @@ echo "Target release name: "$RELEASE_NAME
 echo "Target acr: ${ACR_NAME}"
 echo "Target agent repo name: ${IMAGE_REPO}"
 
-imagetag=$RELEASE_NAME$IMAGE_TAG_SUFFIX
+IMAGE_TAG=$RELEASE_NAME$IMAGE_TAG_SUFFIX
 
-echo "Target Agent image tag: "$imagetag
-echo "Creating target agent full image path"
+echo "Target image tag: ${IMAGE_TAG}"
+echo "Creating target full image path"
 
-fullImagePath=${IMAGE_PATH}/${IMAGE_REPO}:${imagetag}
-
-echo "Target image full path: ${fullImagePath}"
-echo "Source image full path: ${CDPX_ACR_RESOURCE_ID}/${CDPX_REPO_NAME}: ${CDPX_IMAGE_TAG}"
-echo "Pushing the image from  ${CDPX_ACR_RESOURCE_ID}/${CDPX_REPO_NAME} to acr: ${ACR_NAME}"
+TARGET_FULL_IMAGE_PATH=${IMAGE_PATH}/${IMAGE_REPO}:${IMAGE_TAG}
 
 CDPX_FULL_IMAGE_PATH=${CDPX_ACR}/artifact/3170cdd2-19f0-4027-912b-1027311691a2/official/${CDPX_REPO_NAME}:${CDPX_IMAGE_TAG}
 
+echo "Target image full path: ${TARGET_FULL_IMAGE_PATH}"
+echo "Source image full path: ${CDPX_FULL_IMAGE_PATH}"
+
+
+
+echo "Logging into az cli"
 az login --identity
 
-echo "Attempt 1:"
-az acr import -r ${CDPX_ACR_RESOURCE_ID} --source ${CDPX_REPO_NAME}:${CDPX_IMAGE_TAG} -n ${ACR_NAME} -t ${fullImagePath} 
 
-echo "Attempt 2:"
-az acr import --source ${CDPX_FULL_IMAGE_PATH} -n ${ACR_NAME} -t ${fullImagePath}
+echo "Pushing the image from  ${CDPX_FULL_IMAGE_PATH} to acr: ${ACR_NAME}"
 
-echo "Attempt 3:"
-az acr import -n ${ACR_NAME} --source ${CDPX_FULL_IMAGE_PATH} -t ${fullImagePath} -u ${ACR_APP_ID} -p ${ACR_APP_SECRET} 
+
+az acr import -n ${ACR_NAME} --source ${CDPX_FULL_IMAGE_PATH} -t ${TARGET_FULL_IMAGE_PATH} -u ${ACR_APP_ID} -p ${ACR_APP_SECRET} 
 
 echo "Pushing the image to acr completed"
 
