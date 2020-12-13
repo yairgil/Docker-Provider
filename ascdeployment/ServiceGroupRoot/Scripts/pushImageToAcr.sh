8825a1545e09231ac48376256f6d1378e2452289
galofir <galofir@microsoft.com>
2020-12-13 11:20:41 +0200

    Added Date Tag to image

--- ascdeployment/ServiceGroupRoot/Scripts/pushImageToAcr.sh
+++ ascdeployment/ServiceGroupRoot/Scripts/pushImageToAcr.sh
@@ -8,6 +8,7 @@ echo "Target release name: "$RELEASE_NAME
 echo "Target acr: ${ACR_NAME}"
 echo "Target agent repo name: ${IMAGE_REPO}"
 
+IMAGE_TAG_SUFFIX=$(date +"%m%d%y_%H%M")
 IMAGE_TAG=$RELEASE_NAME$IMAGE_TAG_SUFFIX
 
 echo "Target image tag: ${IMAGE_TAG}"
@@ -20,18 +21,13 @@ CDPX_FULL_IMAGE_PATH=${CDPX_ACR}/artifact/3170cdd2-19f0-4027-912b-1027311691a2/o
 echo "Target image full path: ${TARGET_FULL_IMAGE_PATH}"
 echo "Source image full path: ${CDPX_FULL_IMAGE_PATH}"
 
-
-
 echo "Logging into az cli"
 az login --identity
 
-
 echo "Pushing the image from  ${CDPX_FULL_IMAGE_PATH} to acr: ${ACR_NAME}"
-
 
 az acr import -n ${ACR_NAME} --source ${CDPX_FULL_IMAGE_PATH} -t ${TARGET_FULL_IMAGE_PATH} -u ${ACR_APP_ID} -p ${ACR_APP_SECRET} 
 
 echo "Pushing the image to acr completed"
-
 echo "Finished: Import linux agent image from cdpx and push to acr: ${ACR_NAME}"
 
