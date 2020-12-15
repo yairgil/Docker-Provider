24b709f9e3c3b18779102b491fc98b87a99d1335
Ganga Mahesh Siddem <gangams@microsoft.com>
2020-12-15 09:42:52 -0800

    Gangams/fix for build release pipeline issue (#476)
    * use isolated cdpx acr
    
    * correct comment

--- .pipelines/get-aad-app-creds-from-kv.sh
+++ .pipelines/get-aad-app-creds-from-kv.sh
@@ -11,6 +11,8 @@ do
            KV) KV=$VALUE ;;
            KVSECRETNAMEAPPID) AppId=$VALUE ;;
            KVSECRETNAMEAPPSECRET) AppSecret=$VALUE ;;
+           KVSECRETNAMECDPXAPPID) CdpxAppId=$VALUE ;;
+           KVSECRETNAMECDPXAPPSECRET) CdpxAppSecret=$VALUE ;;
            *)
     esac
 done
@@ -26,5 +28,17 @@ echo "downloaded the appid from KV:${KV} and KV secret:${AppId}"
 az keyvault secret download --file ~/acrappsecret --vault-name ${KV}  --name ${AppSecret}
 
 echo "downloaded the appsecret from KV:${KV} and KV secret:${AppSecret}"
+
+echo "key vault secret name for cdpx appid:${KVSECRETNAMECDPXAPPID}"
+
+echo "key vault secret name for cdpx appsecret:${KVSECRETNAMECDPXAPPSECRET}"
+
+az keyvault secret download --file ~/cdpxacrappid --vault-name ${KV}  --name ${CdpxAppId}
+
+echo "downloaded the appid from KV:${KV} and KV secret:${CdpxAppId}"
+
+az keyvault secret download --file ~/cdpxacrappsecret --vault-name ${KV}  --name ${CdpxAppSecret}
+
+echo "downloaded the appsecret from KV:${KV} and KV secret:${CdpxAppSecret}"
 
 echo "end: get app id and secret from specified key vault"
