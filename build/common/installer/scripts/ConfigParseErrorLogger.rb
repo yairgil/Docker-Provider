548799caff9a5104ac9c92612cd7de574b1deeaa
Ganga Mahesh Siddem <gangams@microsoft.com>
2020-12-19 13:15:28 -0800

    fix oj path

--- build/common/installer/scripts/ConfigParseErrorLogger.rb
+++ build/common/installer/scripts/ConfigParseErrorLogger.rb
@@ -2,7 +2,7 @@
 # frozen_string_literal: true
 
 class ConfigParseErrorLogger
-  require_relative "../../../../source/plugins/ruby/oj/oj"
+  require_relative "microsoft/omsagent/plugin/oj/oj"
 
   def initialize
   end
