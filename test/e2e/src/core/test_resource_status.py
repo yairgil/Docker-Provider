94b13803a86947aa85fba64e48200046fc2027f9
Ganga Mahesh Siddem <gangams@microsoft.com>
2021-02-10 13:16:14 -0800

    wip

--- test/e2e/src/core/test_resource_status.py
+++ test/e2e/src/core/test_resource_status.py
@@ -4,8 +4,8 @@ import constants
 from kubernetes import client, config
 # from kubernetes_pod_utility import get_pod_list
 from results_utility import append_result_output
-from helper import check_kubernetes_deployment_status
-from helper import check_kubernetes_daemonset_status
+# from helper import check_kubernetes_deployment_status
+# from helper import check_kubernetes_daemonset_status
 # from helper import check_kubernetes_pods_status
 # from helper import check_kubernetes_pod_logs
 # from helper import check_kubernetes_pods_status, check_namespace_status
@@ -28,10 +28,10 @@ def test_resource_status(env_dict):
         pytest.fail("Error loading the in-cluster config: " + str(e))
 
     # checking the deployment status
-    check_kubernetes_deployment_status(constants.AGENT_RESOURCES_NAMESPACE, constants.AGENT_DEPLOYMENT_NAME, env_dict['TEST_AGENT_LOG_FILE'])
+    # check_kubernetes_deployment_status(constants.AGENT_RESOURCES_NAMESPACE, constants.AGENT_DEPLOYMENT_NAME, env_dict['TEST_AGENT_LOG_FILE'])
 
     # checking the daemonset status
-    check_kubernetes_daemonset_status(constants.AGENT_RESOURCES_NAMESPACE, constants.AGENT_DAEMONSET_NAME, env_dict['TEST_AGENT_LOG_FILE'])    
+    # check_kubernetes_daemonset_status(constants.AGENT_RESOURCES_NAMESPACE, constants.AGENT_DAEMONSET_NAME, env_dict['TEST_AGENT_LOG_FILE'])    
     
     # checking deployment pod status
     # check_kubernetes_pods_status(constants.AGENT_RESOURCES_NAMESPACE, constants.AGENT_DEPLOYMENT_PODS_LABEL_SELECTOR, env_dict['TEST_AGENT_LOG_FILE'])
