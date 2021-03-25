5e6b3b3e3a9a01d1c74997b245e767a8ffc555cb
Ganga Mahesh Siddem <gangams@microsoft.com>
2021-03-25 15:45:17 -0700

    add other rollout regions

--- deployment/ServiceGroupRoot/Scripts/pushChartToAcr.sh
+++ deployment/ServiceGroupRoot/Scripts/pushChartToAcr.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
 
 export HELM_EXPERIMENTAL_OCI=1
+export MCR_NAME="mcr.microsoft.com"
+export REPO_TYPE="preview"
+
+# repo paths for SDP roll-out
+# canary region
+export CANARY_REGION_REPO_PATH="azuremonitor/containerinsights/canary/${REPO_TYPE}/azuremonitor-containers"
+# pilot region
+export PILOT_REGION_REPO_PATH="azuremonitor/containerinsights/prod1/${REPO_TYPE}/azuremonitor-containers"
+# medium load regions (1)
+export PROD2_REGION_REPO_PATH="azuremonitor/containerinsights/prod2/${REPO_TYPE}/azuremonitor-containers"
+# medium load regions (2)
+export PROD3_REGION_REPO_PATH="azuremonitor/containerinsights/prod3/${REPO_TYPE}/azuremonitor-containers"
+# high load regions (1)
+export PROD4_REGION_REPO_PATH="azuremonitor/containerinsights/prod4/${REPO_TYPE}/azuremonitor-containers"
+# FF
+export PROD5_REGION_REPO_PATH="azuremonitor/containerinsights/prod5/${REPO_TYPE}/azuremonitor-containers"
 
 echo "START - Release stage : ${RELEASE_STAGE}"
 
@@ -15,41 +31,103 @@ echo "login to acr:${ACR_NAME} completed: ${ACR_NAME}"
 case $RELEASE_STAGE in
 
   Canary)
-    echo -n "Release stage - Canary"
-    echo "start: push the chart version: ${CHART_VERSION} to acr repo: ${ACR_NAME}"
-    echo "save the chart locally with acr full path"
-    helm chart save charts/azuremonitor-containers/ ${ACR_NAME}/${REPO_PATH}:${CHART_VERSION}
-    echo "pushing the helm chart to ACR: ${ACR}"
-    helm chart push ${ACR_NAME}/${REPO_PATH}:${CHART_VERSION}
-    echo "end: push the chart version: ${CHART_VERSION} to acr repo: ${ACR_NAME}"
+    echo -n "start: Release stage - Canary"
+    acrFullPath=${ACR_NAME}/public/${CANARY_REGION_REPO_PATH}:${CHART_VERSION}    
+    echo "start: push the chart : ${acrFullPath}"
+    echo "save the chart locally with acr full path: ${acrFullPath}"
+    helm chart save charts/azuremonitor-containers/ $acrFullPath
+    echo "start: pushing the helm chart to ACR: ${acrFullPath}"
+    helm chart push $acrFullPath
+    echo "end: push the chart : ${acrFullPath}"    
+    echo -n "end: Release stage - Canary"
     ;;
 
-  Pilot)
-    echo -n "Release stage - Pilot"
-    echo "Pull canary region chart from MCR"
+  Pilot | Prod1)
+    # prod 1
+    echo -n "start: Release stage - Pilot"    
+    mcrFullPath=${MCR_NAME}/${CANARY_REGION_REPO_PATH}:${CHART_VERSION}            
+    echo "Pulling canary region chart from MCR:${mcrFullPath} to push Pilot regions"
+    helm chart pull ${mcrFullPath}
+    echo "Exporting chart"    
+    helm chart export ${mcrFullPath}    
+    acrFullPath=${ACR_NAME}/public/${PILOT_REPO_PATH}:${CHART_VERSION} 
+    echo "save the chart locally with acr full path: ${acrFullPath}"    
+    helm chart save azuremonitor-containers/ ${acrFullPath} 
+    echo "start: push the chart version: ${acrFullPath}"
+    helm chart push ${acrFullPath} 
+    echo "end: push the chart version: ${acrFullPath}"
+    echo -n "end: Release stage - Pilot"    
     ;;
 
-  Prod1)
-    echo -n "Release stage - Prod1"
-    echo "Pull Prod1 region chart from MCR"
+  Prod2)
+    # prod 2
+    echo -n "start: Release stage - Medium Low Laod Regions - Prod2"    
+    mcrFullPath=${MCR_NAME}/${PILOT_REGION_REPO_PATH}:${CHART_VERSION}
+    echo "Pull Prod1 region chart from MCR:${mcrFullPath} to push to Prod2 regions"
+    helm chart pull ${mcrFullPath}
+    echo "Exporting chart"    
+    helm chart export ${mcrFullPath}
+    acrFullPath=${ACR_NAME}/public/${PROD2_REPO_PATH}:${CHART_VERSION}
+    echo "save the chart locally with acr full path: ${acrFullPath}"    
+    helm chart save azuremonitor-containers/ ${acrFullPath}    
+    echo "start: push the chart version: ${acrFullPath}"
+    helm chart push ${acrFullPath} 
+    echo "end: push the chart version: ${acrFullPath}"
+    echo -n "end: Release stage - Medium Low Laod Regions - Prod2"    
     ;;
    
-  Prod2)
-    echo -n "Release stage - Prod2"
-    echo "Pull Prod2 region chart from MCR"
+  Prod3)
+    # prod 3
+    echo -n "start: Release stage - Medium High Laod Regions - Prod3"
+    echo "Pull Prod2 region chart from MCR to push to Prod3 regions"
+    mcrFullPath=${MCR_NAME}/${PROD2_REGION_REPO_PATH}:${CHART_VERSION}
+    helm chart pull ${mcrFullPath}
+    echo "Exporting chart"    
+    helm chart export ${mcrFullPath}
+    echo "save the chart locally with acr full path"
+    acrFullPath=${ACR_NAME}/public/${PROD3_REPO_PATH}:${CHART_VERSION}
+    helm chart save azuremonitor-containers/ ${acrFullPath}    
+    echo "start: push the chart version: ${acrFullPath}"
+    helm chart push ${acrFullPath} 
+    echo "end: push the chart version: ${acrFullPath}"        
+    echo -n "end: Release stage - Medium High Laod Regions - Prod3"
     ;;
 
-  Prod3)
-    echo -n "Release stage - Prod3"
-    echo "Pull Prod3 region chart from MCR"
+  Prod4)
+    # prod 4    
+    echo -n "start: Release stage - High Laod Regions - Prod4"
+    echo "Pull Prod3 region chart from MCR to push to Prod4 regions"
+    mcrFullPath=${MCR_NAME}/${PROD3_REGION_REPO_PATH}:${CHART_VERSION}    
+    helm chart pull ${mcrFullPath}
+    echo "Exporting chart"    
+    helm chart export ${mcrFullPath}
+    acrFullPath=${ACR_NAME}/public/${PROD4_REPO_PATH}:${CHART_VERSION}
+    echo "save the chart locally with acr full path: ${acrFullPath}"    
+    helm chart save azuremonitor-containers/ ${acrFullPath}    
+    echo "start: push the chart version: ${acrFullPath}"
+    helm chart push ${acrFullPath} 
+    echo "end: push the chart version: ${acrFullPath}"  
+    echo -n "end: Release stage - High Laod Regions - Prod4"  
     ;;  
 
-  FF)
-    echo -n "Release stage - FF"
-    echo "Pull FF region chart from MCR"
+  FF | Prod5)
+    # prod 5
+    echo -n "start: Release stage - FF"    
+    mcrFullPath=${MCR_NAME}/${PROD4_REGION_REPO_PATH}:${CHART_VERSION}        
+    echo "Pull Prod4 region chart from MCR:${mcrFullPath} to push to Prod5 regions"    
+    helm chart pull ${mcrFullPath}
+    echo "Exporting chart"    
+    helm chart export ${mcrFullPath}
+    echo "save the chart locally with acr full path"
+    acrFullPath=${ACR_NAME}/public/${PROD5_REPO_PATH}:${CHART_VERSION}
+    helm chart save azuremonitor-containers/ ${acrFullPath}    
+    echo "start: push the chart version: ${acrFullPath}"
+    helm chart push ${acrFullPath} 
+    echo "end: push the chart version: ${acrFullPath}"   
+    echo -n "end: Release stage - FF"     
     ;;    
 
-  MC)
+  MC | Prod6)
     echo -n "Release stage - MC"
     echo "Pull MC region chart from MCR"
     ;;    
