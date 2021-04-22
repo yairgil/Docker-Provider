02652c5ccb7563ae0d45ea6b721f22c0eff98a6c
Rashmi Chandrashekar <rashmy@microsoft.com>
2021-04-21 19:23:45 -0700

    adding filter plugin

--- build/windows/Makefile.ps1
+++ build/windows/Makefile.ps1
@@ -182,9 +182,11 @@ Write-Host("successfully copied installer files conf and scripts from :" + $inst
 
 $rubyplugindir = Join-Path -Path $rootdir -ChildPath "source\plugins\ruby"
 $outmdmpluginpath = Join-Path -Path $rubyplugindir -ChildPath "\out_mdm.rb"
+$filtertelegraftomdm = Join-Path -Path $rubyplugindir -ChildPath "\filter_telegraf2mdm.rb"
 # $destdir = Join-Path -Path $publishdir -ChildPath "\plugins\ruby"
 Write-Host("copying ruby source files from :" + $rubyplugindir + "  to  :" + $publishdir + " ...")
 Copy-Item  -Path $outmdmpluginpath  -Destination $publishdir -Force
+Copy-Item  -Path $filtertelegraftomdm  -Destination $publishdir -Force
 Write-Host("successfully copied ruby source files from :" + $rubyplugindir + "  to  :" + $publishdir + " ") -ForegroundColor Green
 
 Set-Location $currentdir
