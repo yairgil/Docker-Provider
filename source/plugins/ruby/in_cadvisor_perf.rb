4672f85a023043d859876949ff84126bac5266d0
Ganga Mahesh Siddem <gangams@microsoft.com>
2021-05-11 15:07:01 -0700

    wip

--- source/plugins/ruby/in_cadvisor_perf.rb
+++ source/plugins/ruby/in_cadvisor_perf.rb
@@ -38,10 +38,6 @@ module Fluent::Plugin
         @condition = ConditionVariable.new
         @mutex = Mutex.new
         @thread = Thread.new(&method(:run_periodic))
-        if !ENV["AAD_MSI_AUTH_ENABLE"].nil? && !ENV["AAD_MSI_AUTH_ENABLE"].empty? && ENV["AAD_MSI_AUTH_ENABLE"].downcase == "true"
-          @aad_msi_auth_enable = true
-        end              
-        $log.info("in_cadvisor_perf::start: aad auth enable:#{@aad_msi_auth_enable}")
       end
     end
 
