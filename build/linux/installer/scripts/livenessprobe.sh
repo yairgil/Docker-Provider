7c5087f7f57d8b3e3a1f430ab3dfca19f8f888a9
David Michelman <daweim0@gmail.com>
2021-05-19 16:22:27 -0700

    partially disabled telegraf liveness probe check, we'll still have telemetry but the probe won't fail if telegraf isn't running (#561)

--- build/linux/installer/scripts/livenessprobe.sh
+++ build/linux/installer/scripts/livenessprobe.sh
@@ -30,9 +30,9 @@ fi
 (ps -ef | grep telegraf | grep -v "grep")
 if [ $? -ne 0 ]
 then
- echo "Telegraf is not running" > /dev/termination-log
+ # echo "Telegraf is not running" > /dev/termination-log
  echo "Telegraf is not running (controller: ${CONTROLLER_TYPE}, container type: ${CONTAINER_TYPE})" > /dev/write-to-traces  # this file is tailed and sent to traces
- exit 1
+ # exit 1
 fi
 
 if [ -s "inotifyoutput.txt" ]
