c4094e5aa7cc53d2ee1123567822bb5baeb780bf
Ganga Mahesh Siddem <gangams@microsoft.com>
2021-06-06 12:34:32 -0700

    ama build with qos fixes

--- kubernetes/linux/setup.sh
+++ kubernetes/linux/setup.sh
@@ -13,7 +13,7 @@ sed -i -e 's/# en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen && \
 #wget https://github.com/microsoft/Docker-Provider/releases/download/05172021-oneagent/azure-mdsd_1.10.1-build.master.213_x86_64.deb
 #mdsd build with 60s ods http timeout
 # make sure to update to use official build
-wget https://github.com/microsoft/Docker-Provider/raw/gangams/ci-aad-auth-msi/oneagent-dev/azure-mdsd_1.11.0-build.develop.2033_x86_64.deb
+wget https://github.com/microsoft/Docker-Provider/raw/gangams/ci-aad-auth-msi/oneagent-dev/azure-mdsd_1.11.0-build.develop.2047_x86_64.deb
 
 /usr/bin/dpkg -i $TMPDIR/azure-mdsd*.deb
 cp -f $TMPDIR/mdsd.xml /etc/mdsd.d
