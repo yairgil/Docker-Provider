8198b7ca557bca47f83dd9114e28deb109422ecb
Ganga Mahesh Siddem <gangams@microsoft.com>
2021-06-08 09:21:27 -0700

    mdsd official build for stage2 release

--- kubernetes/linux/setup.sh
+++ kubernetes/linux/setup.sh
@@ -9,11 +9,8 @@ sed -i -e 's/# en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen && \
     dpkg-reconfigure --frontend=noninteractive locales && \
     update-locale LANG=en_US.UTF-8
 
-#install oneagent - Official bits (05/17/2021)
-#wget https://github.com/microsoft/Docker-Provider/releases/download/05172021-oneagent/azure-mdsd_1.10.1-build.master.213_x86_64.deb
-#mdsd build with 60s ods http timeout
-# make sure to update to use official build
-wget https://github.com/microsoft/Docker-Provider/raw/gangams/ci-aad-auth-msi/oneagent-dev/azure-mdsd_1.11.0-build.develop.2047_x86_64.deb
+#install oneagent - Official bits (06/07/2021)
+wget https://github.com/microsoft/Docker-Provider/releases/download/06072021-oneagent/azure-mdsd_1.11.0-build.develop.228_x86_64.deb
 
 /usr/bin/dpkg -i $TMPDIR/azure-mdsd*.deb
 cp -f $TMPDIR/mdsd.xml /etc/mdsd.d
