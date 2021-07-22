91100effdb353c0054882d5d38738b56b3ef8a75
Ganga Mahesh Siddem <gangams@microsoft.com>
2021-07-22 16:33:16 -0700

    minor update

--- .pipelines/release-agent.sh
+++ .pipelines/release-agent.sh
@@ -2,18 +2,28 @@
 
 # Note - This script used in the pipeline as inline script
 
-#Name of the ACR for ciprod & cidev images
-AGENT_RELEASE=cidev
-LINUX_AGENT_IMAGE_TAG_SUFFIX=07222021
+# These are plain pipeline variable which can be modified anyone in the team
+# AGENT_RELEASE=cidev
+# AGENT_IMAGE_TAG_SUFFIX=07222021
 
+#Name of the ACR for ciprod & cidev images
 ACR_NAME=containerinsightsprod.azurecr.io
-AGENT_IMAGE_FULL_PATH=${ACR_NAME}/public/azuremonitor/containerinsights/${AGENT_RELEASE}:${AGENT_RELEASE}${LINUX_AGENT_IMAGE_TAG_SUFFIX}
+AGENT_IMAGE_FULL_PATH=${ACR_NAME}/public/azuremonitor/containerinsights/${AGENT_RELEASE}:${AGENT_RELEASE}${AGENT_IMAGE_TAG_SUFFIX}
 AGENT_IMAGE_TAR_FILE_NAME=agentimage.tar.gz
 
+if [ -z $AGENT_IMAGE_TAG_SUFFIX ]; then
+  echo "-e error value of AGENT_RELEASE variable shouldnt be empty"
+  exit 1
+fi
+
+if [ -z $AGENT_RELEASE ]; then
+  echo "-e error AGENT_RELEASE shouldnt be empty"
+  exit 1
+fi
 
 echo "ACR NAME - ${ACR_NAME}"
 echo "AGENT RELEASE -  ${AGENT_RELEASE}"
-echo "AGENT IMAGE TAG SUFFIX -  ${LINUX_AGENT_IMAGE_TAG_SUFFIX}"
+echo "AGENT IMAGE TAG SUFFIX -  ${AGENT_IMAGE_TAG_SUFFIX}"
 echo "AGENT IMAGE FULL PATH -  ${AGENT_IMAGE_FULL_PATH}"
 echo "AGENT IMAGE TAR FILE PATH -  ${AGENT_IMAGE_TAR_FILE_NAME}"
 
@@ -21,7 +31,7 @@ echo "loading linuxagent image tarball"
 IMAGE_NAME=$(docker load -i ${AGENT_IMAGE_TAR_FILE_NAME})
 echo IMAGE_NAME: $IMAGE_NAME
 if [ $? -ne 0 ]; then
-  echo "Error, on loading linux agent tarball from ${AGENT_IMAGE_TAR_FILE_NAME}"
+  echo "-e error, on loading linux agent tarball from ${AGENT_IMAGE_TAR_FILE_NAME}"
   echo "** Please check if this caused due to build error **"
   exit 1
 else
@@ -38,7 +48,7 @@ echo "tagging the image $IMAGE_NAME as ${AGENT_IMAGE_FULL_PATH}"
 docker tag $IMAGE_NAME $AGENT_IMAGE_FULL_PATH
 
 if [ $? -ne 0 ]; then
-  echo "Error, tagging the image $IMAGE_NAME as ${AGENT_IMAGE_FULL_PATH}"
+  echo "-e error  tagging the image $IMAGE_NAME as ${AGENT_IMAGE_FULL_PATH}"
   exit 1
 else
   echo "successfully tagged the image $IMAGE_NAME as ${AGENT_IMAGE_FULL_PATH}"
@@ -48,7 +58,7 @@ fi
 echo "logging to acr: ${ACR_NAME}"
 az acr login --name ${ACR_NAME}
 if [ $? -ne 0 ]; then
-  echo "Error, log into acr failed: ${ACR_NAME}"
+  echo "-e error  log into acr failed: ${ACR_NAME}"
   exit 1
 else
  echo "successfully logged into acr:${ACR_NAME}"
@@ -57,7 +67,7 @@ fi
 echo "pushing ${AGENT_IMAGE_FULL_PATH}"
 docker push ${AGENT_IMAGE_FULL_PATH}
 if [ $? -ne 0 ]; then
-  echo "Error, on pushing the image ${AGENT_IMAGE_FULL_PATH}"
+  echo "-e error  on pushing the image ${AGENT_IMAGE_FULL_PATH}"
   exit 1
 else
   echo "Successfully pushed the image ${AGENT_IMAGE_FULL_PATH}"
