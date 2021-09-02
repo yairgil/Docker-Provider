367d6302c9da52bf7b4bd9447f6c785f3a78edb0
bragi92 <kaveesh.dubey@gmail.com>
2021-09-02 07:58:01 -0700

    test

--- kubernetes/windows/main.cmd
+++ kubernetes/windows/main.cmd
@@ -1,8 +1,6 @@
 echo "Finishing up with main.ps1..."
 echo "Adding in a second line"
 
-:loop
+C:\opt\fluent-bit\bin\fluent-bit.exe -c "C:\etc\fluent-bit\fluent-bit.conf" -e "C:\opt\omsagentwindows\out_oms.so"
 
-echo "keeping main.cmd running"
-
-goto loop
+@REM fluentd --reg-winsvc i --reg-winsvc-auto-start --winsvc-name fluentdwinaks --reg-winsvc-fluentdopt '-c C:/etc/fluent/fluent.conf -o C:/etc/fluent/fluent.log'
