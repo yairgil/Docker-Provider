a9b004263aef4ce8127bc882836d6011dc0bc936
Ganga Mahesh Siddem <gangams@microsoft.com>
2021-09-25 14:18:27 -0700

    optimize windows agent liveness probe

--- build/windows/installer/livenessprobe/livenessprobe.cpp
+++ build/windows/installer/livenessprobe/livenessprobe.cpp
@@ -10,30 +10,34 @@
 #include <tlhelp32.h>
 #include <tchar.h>
 
-#define SUCCESS                            0x00000000
-#define NO_FLUENT_BIT_PROCESS              0x00000001
-#define FILESYSTEM_WATCHER_FILE_EXISTS     0x00000002
-#define CERTIFICATE_RENEWAL_REQUIRED       0x00000003
-#define FLUENTDWINAKS_SERVICE_NOT_RUNNING  0x00000004
-#define UNEXPECTED_ERROR                   0xFFFFFFFF
+#define SUCCESS 0x00000000
+#define NO_FLUENT_BIT_PROCESS 0x00000001
+#define FILESYSTEM_WATCHER_FILE_EXISTS 0x00000002
+#define CERTIFICATE_RENEWAL_REQUIRED 0x00000003
+#define FLUENTDWINAKS_SERVICE_NOT_RUNNING 0x00000004
+#define UNEXPECTED_ERROR 0xFFFFFFFF
 
 /*
   check if the process running or not for given exe file name
 */
-bool IsProcessRunning(const wchar_t * const executableName) {
+bool IsProcessRunning(const wchar_t *const executableName)
+{
     PROCESSENTRY32 entry;
     entry.dwSize = sizeof(PROCESSENTRY32);
 
     const auto snapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, NULL);
 
-    if (!Process32First(snapshot, &entry)) {
+    if (!Process32First(snapshot, &entry))
+    {
         CloseHandle(snapshot);
         wprintf_s(L"ERROR:IsProcessRunning::Process32First failed");
         return false;
     }
 
-    do {
-        if (!_wcsicmp(entry.szExeFile, executableName)) {
+    do
+    {
+        if (!_wcsicmp(entry.szExeFile, executableName))
+        {
             CloseHandle(snapshot);
             return true;
         }
@@ -46,7 +50,7 @@ bool IsProcessRunning(const wchar_t * const executableName) {
 /*
   check if the file exists
 */
-bool IsFileExists(const wchar_t* const fileName)
+bool IsFileExists(const wchar_t *const fileName)
 {
     DWORD dwAttrib = GetFileAttributes(fileName);
     return dwAttrib != INVALID_FILE_SIZE;
@@ -55,34 +59,37 @@ bool IsFileExists(const wchar_t* const fileName)
 /*
  Get the status of the service for given service name
 */
-int GetServiceStatus(const wchar_t * const serivceName)
+int GetServiceStatus(const wchar_t *const serivceName)
 {
     SC_HANDLE theService, scm;
     SERVICE_STATUS_PROCESS ssStatus;
     DWORD dwBytesNeeded;
 
     scm = OpenSCManager(nullptr, nullptr, SC_MANAGER_ENUMERATE_SERVICE);
-    if (!scm) {
+    if (!scm)
+    {
         wprintf_s(L"ERROR:GetServiceStatus::OpenSCManager failed");
         return UNEXPECTED_ERROR;
     }
 
     theService = OpenService(scm, serivceName, SERVICE_QUERY_STATUS);
-    if (!theService) {
+    if (!theService)
+    {
         CloseServiceHandle(scm);
         wprintf_s(L"ERROR:GetServiceStatus::OpenService failed");
         return UNEXPECTED_ERROR;
     }
 
     auto result = QueryServiceStatusEx(theService, SC_STATUS_PROCESS_INFO,
-        reinterpret_cast<LPBYTE>(&ssStatus), sizeof(SERVICE_STATUS_PROCESS),
-        &dwBytesNeeded);
+                                       reinterpret_cast<LPBYTE>(&ssStatus), sizeof(SERVICE_STATUS_PROCESS),
+                                       &dwBytesNeeded);
 
     CloseServiceHandle(theService);
     CloseServiceHandle(scm);
 
-    if (result == 0) {
-         wprintf_s(L"ERROR:GetServiceStatus:QueryServiceStatusEx failed");
+    if (result == 0)
+    {
+        wprintf_s(L"ERROR:GetServiceStatus:QueryServiceStatusEx failed");
         return UNEXPECTED_ERROR;
     }
 
@@ -91,9 +98,10 @@ int GetServiceStatus(const wchar_t * const serivceName)
 /**
  <exe> <servicename> <filesystemwatcherfilepath> <certificaterenewalpath>
 **/
-int _tmain(int argc, wchar_t * argv[])
+int _tmain(int argc, wchar_t *argv[])
 {
-    if( argc < 5) {
+    if (argc < 5)
+    {
         wprintf_s(L"ERROR:unexpected number arguments and expected is 5");
         return UNEXPECTED_ERROR;
     }
@@ -108,8 +116,8 @@ int _tmain(int argc, wchar_t * argv[])
 
     if (dwStatus != SERVICE_RUNNING)
     {
-       wprintf_s(L"ERROR:Service:%s is not running\n", argv[2]);
-       return FLUENTDWINAKS_SERVICE_NOT_RUNNING;
+        wprintf_s(L"ERROR:Service:%s is not running\n", argv[2]);
+        return FLUENTDWINAKS_SERVICE_NOT_RUNNING;
     }
 
     if (IsFileExists(argv[3]))
