914839844a321d52f9bcc391cc6a7c63156c3ffa
Ganga Mahesh Siddem <gangams@microsoft.com>
2021-10-21 17:21:36 -0700

    fix pr feedback

--- test/e2e/src/tests/test_rs_workflows.py
+++ test/e2e/src/tests/test_rs_workflows.py
@@ -39,9 +39,7 @@ def test_rs_workflows(env_dict):
 
 
     waitTimeSeconds = env_dict['AGENT_WAIT_TIME_SECS']
-    print("start: waiting for seconds: {} for agent workflows to get emitted".format(waitTimeSeconds))
     time.sleep(int(waitTimeSeconds))
-    print("complete: waiting for seconds: {} for agent workflows to get emitted".format(waitTimeSeconds))
 
     isOMSBaseAgent = env_dict.get('USING_OMSAGENT_BASE_AGENT')
     agentLogPath = constants.AGENT_FLUENTD_LOG_PATH
