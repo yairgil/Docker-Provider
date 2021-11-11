7d5fdbce975043d4c7b7a667c21e9cd523a035b5
Ganga Mahesh Siddem <gangams@microsoft.com>
2021-11-10 18:36:15 -0800

    imds endpoint for windows

--- source/plugins/ruby/constants.rb
+++ source/plugins/ruby/constants.rb
@@ -110,7 +110,7 @@ class Constants
   CONTAINER_INVENTORY_DATA_TYPE = "CONTAINER_INVENTORY_BLOB"
   CONTAINER_NODE_INVENTORY_DATA_TYPE = "CONTAINER_NODE_INVENTORY_BLOB"
   PERF_DATA_TYPE = "LINUX_PERF_BLOB"
-  INSIGHTS_METRICS_DATA_TYPE = "INSIGHTS_METRICS_BLOB"  
+  INSIGHTS_METRICS_DATA_TYPE = "INSIGHTS_METRICS_BLOB"
   KUBE_SERVICES_DATA_TYPE = "KUBE_SERVICES_BLOB"
   KUBE_POD_INVENTORY_DATA_TYPE = "KUBE_POD_INVENTORY_BLOB"
   KUBE_NODE_INVENTORY_DATA_TYPE = "KUBE_NODE_INVENTORY_BLOB"
@@ -123,13 +123,17 @@ class Constants
 
   #ContainerInsights Extension (AMCS)
   CI_EXTENSION_NAME = "ContainerInsights"
-  CI_EXTENSION_VERSION = "1"  
+  CI_EXTENSION_VERSION = "1"
   #Current CI extension config size is ~5KB and going with 20KB to handle any future scenarios
   CI_EXTENSION_CONFIG_MAX_BYTES = 20480
-  ONEAGENT_FLUENT_SOCKET_NAME = "/var/run/mdsd/default_fluent.socket" 
+  ONEAGENT_FLUENT_SOCKET_NAME = "/var/run/mdsd/default_fluent.socket"
   #Tag prefix for output stream
   EXTENSION_OUTPUT_STREAM_ID_TAG_PREFIX = "dcr-"
 
   LINUX_LOG_PATH = $in_unit_test.nil? ? "/var/opt/microsoft/docker-cimprov/log/" : "./"
   WINDOWS_LOG_PATH = $in_unit_test.nil? ? "/etc/omsagentwindows/" : "./"
+  DEFAULT_IMDS_ENDPOINT_HOST = "169.254.169.254"
+  # make sure both AKS and Arc K8s have same contract for imds host for windows
+  # this makes easy transition for windows AMA since this being used for Arc in AMA already
+  IMDS_ENDPOINT_HOST_WINDOWS_SIDECAR = "localhost:40342"
 end
--- source/plugins/ruby/extension_utils.rb
+++ source/plugins/ruby/extension_utils.rb
@@ -2,26 +2,35 @@
 #!/usr/local/bin/ruby
 # frozen_string_literal: true
 
-require_relative "extension"                    
+require_relative "extension"
+require_relative "constants"
 
 class ExtensionUtils
-    class << self        
-        def getOutputStreamId(dataType)  
-          outputStreamId = "" 
+    class << self
+        def getOutputStreamId(dataType)
+          outputStreamId = ""
           begin
             if !dataType.nil? && !dataType.empty?
-              outputStreamId = Extension.instance.get_output_stream_id(dataType)  
+              outputStreamId = Extension.instance.get_output_stream_id(dataType)
               $log.info("ExtensionUtils::getOutputStreamId: got streamid: #{outputStreamId} for datatype: #{dataType}")
-            else           
+            else
               $log.warn("ExtensionUtils::getOutputStreamId: dataType shouldnt be nil or empty")
-            end            
+            end
           rescue => errorStr
             $log.warn("ExtensionUtils::getOutputStreamId: failed with an exception: #{errorStr}")
-          end    
-          return outputStreamId     
-        end 
-        def isAADMSIAuthMode() 
+          end
+          return outputStreamId
+        end
+        def isAADMSIAuthMode()
           return !ENV["AAD_MSI_AUTH_MODE"].nil? && !ENV["AAD_MSI_AUTH_MODE"].empty? && ENV["AAD_MSI_AUTH_MODE"].downcase == "true"
-        end        
+        end
+        def getIMDSEndpointHost()
+           imdsEndpointHost = Constants::DEFAULT_IMDS_ENDPOINT_HOST
+            os_type = ENV["OS_TYPE"]
+            if !os_type.nil? && !os_type.empty? && os_type.strip.casecmp("windows") == 0
+                imdsEndpointHost = Constants::IMDS_ENDPOINT_HOST_WINDOWS_SIDECAR
+            end
+           return imdsEndpointHost
+        end
     end
 end
