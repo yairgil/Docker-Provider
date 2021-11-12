1736d40ac87783277acdd16c585dcde2e6258ec6
Sarah Peiffer <sarahpeiffer@microsoft.com>
2021-11-11 17:53:18 -0800

    use configmap values for fluent-bit.conf where necessary and make necessary files common

--- kubernetes/windows/main.ps1
+++ kubernetes/windows/main.ps1
@@ -297,6 +297,13 @@ function Set-EnvironmentVariables {
     # run config parser
     ruby /opt/omsagentwindows/scripts/ruby/tomlparser.rb
     .\setenv.ps1
+    
+    #Parse the configmap to set the right environment variables for agent config.
+    ruby /opt/omsagentwindows/scripts/ruby/tomlparser-agent-config.rb
+    .\setagentenv.ps1
+
+    #Replace placeholders in fluent-bit.conf
+    ruby /opt/omsagentwindows/scripts/ruby/td-agent-bit-conf-customizer.rb
 
     # run mdm config parser
     ruby /opt/omsagentwindows/scripts/ruby/tomlparser-mdm-metrics-config.rb
