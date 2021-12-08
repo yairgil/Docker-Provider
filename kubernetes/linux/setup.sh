162013e208f9182d4441d8b288661e1fb8b8b778
Ganga Mahesh Siddem <gangams@microsoft.com>
2021-12-07 16:34:06 -0800

    fix download url

--- kubernetes/linux/setup.sh
+++ kubernetes/linux/setup.sh
@@ -9,8 +9,9 @@ sed -i -e 's/# en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen && \
     dpkg-reconfigure --frontend=noninteractive locales && \
     update-locale LANG=en_US.UTF-8
 
-#install oneagent - Official bits (10/7/2021)
-wget https://github.com/microsoft/Docker-Provider/releases/download/1.15.0-build.develop/azure-mdsd_1.14.2-build.master.300_x86_64.deb
+#install oneagent - Official bits (12/7/2021)
+wget https://github.com/microsoft/Docker-Provider/releases/download/1.14.2/azure-mdsd_1.14.2-build.master.300_x86_64.deb
+
 
 /usr/bin/dpkg -i $TMPDIR/azure-mdsd*.deb
 cp -f $TMPDIR/mdsd.xml /etc/mdsd.d
