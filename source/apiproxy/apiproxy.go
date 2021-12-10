e808e83e895b12ca2678ec6b9e484a6148f3e0b3
Ganga Mahesh Siddem <gangams@microsoft.com>
2021-12-09 21:03:51 -0800

    wip

--- source/apiproxy/apiproxy.go
+++ source/apiproxy/apiproxy.go
@@ -4,12 +4,14 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
 	"os"
 	"sync"
 	"time"
 
 	v1 "k8s.io/api/core/v1"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/api/meta"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/watch"
@@ -48,6 +50,7 @@ var (
 func listAndWatchPods() {
 	for {
 		if PodsResourceVersion == "" {
+			startTime := time.Now()
 			var podList *v1.PodList
 			var err error
 			podList, err = ClientSet.CoreV1().Pods("").List(context.TODO(), metav1.ListOptions{Limit: POD_CHUNK_SIZE})
@@ -71,6 +74,7 @@ func listAndWatchPods() {
 					Log("apiproxy::listAndWatchPods::get pods failed with an error : %s", err.Error())
 					continueToken = ""
 					podList = nil
+					//TODO - do we need retry incase of chunked api call failure??
 				} else {
 					Log("apiproxy::listAndWatchPods::pod count : %d \n", len(podList.Items))
 					for _, pod := range podList.Items {
@@ -82,6 +86,8 @@ func listAndWatchPods() {
 					podList = nil
 				}
 			}
+			elapsedTime := time.Since(startTime)
+			Log("apiproxy::listAndWatchPods::List Pods API E2E Latency(seconds): %.2f \n", elapsedTime.Seconds())
 			Log("apiproxy::listAndWatchPods::continue: %s \n", continueToken)
 			Log("apiproxy::listAndWatchPods::resource version: %s \n", PodsResourceVersion)
 		}
@@ -94,44 +100,85 @@ func listAndWatchPods() {
 		Log("apiproxy::listAndWatchPods::Establishing watch connection with ResourceVersion: %s \n", PodsResourceVersion)
 		watcher, err := ClientSet.CoreV1().Pods(v1.NamespaceAll).Watch(context.Background(), listOptions)
 		if err != nil {
-			Log("apiproxy::listAndWatchPods::watch connection failed and reconnecting Watch after List: %s \n", err.Error())
+			Log("apiproxy::listAndWatchPods::Failed to establish watch connection with ResourceVersion: %s \n", PodsResourceVersion)
+			switch err {
+			case io.EOF:
+				Log("apiproxy::listAndWatchPods::watch connection closed normally: %s \n", err.Error())
+			case io.ErrUnexpectedEOF:
+				Log("apiproxy::listAndWatchPods::Watch closed with unexpected EOF: %s \n", err.Error())
+			default:
+				Log("apiproxy::listAndWatchPods::watch connection failed with an error: %s \n", err.Error())
+			}
 			time.Sleep(time.Millisecond * 500)
-			PodsResourceVersion = "" // validate, is this right thing to do considering performance reasons
-			continue
-		}
-
-		for event := range watcher.ResultChan() {
-			meta, err := meta.Accessor(event.Object)
-			if err != nil {
-				Log("apiproxy::listAndWatchPods::unable to understand watch event: %s", event.Type)
-				continue
+		} else if watcher != nil {
+			for event := range watcher.ResultChan() {
+				switch event.Type {
+				case watch.Added, watch.Modified, watch.Deleted, watch.Bookmark:
+					meta, err := meta.Accessor(event.Object)
+					if err != nil {
+						Log("apiproxy::listAndWatchPods::unable to understand watch event: %s", event.Type)
+						//setting ResourceVersion to "" enforce List Again
+						PodsResourceVersion = ""
+						// We have to abort here because this might cause lastResourceVersion inconsistency by skipping a potential RV with valid data!
+						watcher.Stop()
+					} else {
+						PodsResourceVersion = meta.GetResourceVersion()
+						Log("apiproxy::listAndWatchPods::Event Type: %s  and Received Resource Version: %s \n", event.Type, PodsResourceVersion)
+						if event.Type != watch.Bookmark {
+							pod := event.Object.(*v1.Pod)
+							if event.Type == watch.Deleted {
+								PodCacheRWLock.Lock()
+								delete(PodItemsCache, string(pod.UID))
+								PodCacheRWLock.Unlock()
+							} else {
+								PodCacheRWLock.Lock()
+								PodItemsCache[string(pod.UID)] = getOptimizedPodItem(pod)
+								PodCacheRWLock.Unlock()
+							}
+						}
+					}
+				case watch.Error:
+					Log("apiproxy::listAndWatchPods::Error event received \n")
+					errObject := apierrors.FromObject(event.Object)
+					statusErr, ok := errObject.(*apierrors.StatusError)
+					if !ok {
+						Log("apiproxy::listAndWatchPods::Received an error which is not *metav1.Status but %#+v", event.Object)
+						//retry unknown error
+						watcher.Stop()
+					} else {
+						status := statusErr.ErrStatus
+						retryAfterSeconds := int32(1)
+						statusDelay := time.Duration(retryAfterSeconds) * time.Second
+						if status.Details != nil {
+							retryAfterSeconds = status.Details.RetryAfterSeconds
+							statusDelay = time.Duration(retryAfterSeconds) * time.Second
+						}
+						Log("apiproxy::listAndWatchPods::Received an error with  error status code : %d", status.Code)
+						switch status.Code {
+						case http.StatusGone:
+							//setting ResourceVersion to "" enforce List Again
+							PodsResourceVersion = ""
+							watcher.Stop()
+							time.Sleep(statusDelay)
+						case http.StatusGatewayTimeout, http.StatusInternalServerError:
+							watcher.Stop()
+							time.Sleep(statusDelay)
+						default:
+							watcher.Stop()
+							time.Sleep(statusDelay)
+						}
+						Log("apiproxy::listAndWatchPods:: Stopping watcher and retrying after delay in seconds: %d", retryAfterSeconds)
+					}
+				default:
+					Log("Failed to recognize Event type %q", event.Type)
+					//setting ResourceVersion to "" enforce List Again
+					PodsResourceVersion = ""
+					// We have to abort here because this might cause lastResourceVersion inconsistency by skipping a potential RV with valid data!
+					watcher.Stop()
+				}
 			}
-			PodsResourceVersion = meta.GetResourceVersion()
-			Log("apiproxy::listAndWatchPods::Event receive version: %s \n", PodsResourceVersion)
-			switch event.Type {
-			case watch.Bookmark:
-				Log("apiproxy::listAndWatchPods::BookMark event received \n")
-			case watch.Error:
-				Log("apiproxy::listAndWatchPods::Error event received \n")
-			case watch.Added:
-				pod := event.Object.(*v1.Pod)
-				PodCacheRWLock.Lock()
-				PodItemsCache[string(pod.UID)] = getOptimizedPodItem(pod)
-				PodCacheRWLock.Unlock()
-				Log("apiproxy::listAndWatchPods::Pod %s/%s added \n", pod.ObjectMeta.Namespace, pod.ObjectMeta.Name)
-			case watch.Modified:
-				pod := event.Object.(*v1.Pod)
-				PodCacheRWLock.Lock()
-				PodItemsCache[string(pod.UID)] = getOptimizedPodItem(pod)
-				PodCacheRWLock.Unlock()
-				Log("apiproxy::listAndWatchPods::Pod %s/%s modified \n", pod.ObjectMeta.Namespace, pod.ObjectMeta.Name)
-			case watch.Deleted:
-				pod := event.Object.(*v1.Pod)
-				PodCacheRWLock.Lock()
-				delete(PodItemsCache, string(pod.UID))
-				PodCacheRWLock.Unlock()
-				Log("apiproxy::listAndWatchPods::Pod %s/%s deleted \n", pod.ObjectMeta.Namespace, pod.ObjectMeta.Name)
-			}
+		} else {
+			Log("apiproxy::listAndWatchPods::Watch returned nil watcher for watch connection with ResourceVersion: %s \n", PodsResourceVersion)
 		}
 	}
 }
@@ -230,17 +277,20 @@ func listAndWatchServices() {
 func pods(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case "GET":
-		fmt.Printf("get Pods invoked :%s \n", time.Now().UTC().String())
+		fmt.Printf("apiproxy::pods::get Pods invoked :%s \n", time.Now().UTC().String())
 
 		podsResponse := make(map[string][]PodItem)
 		PodCacheRWLock.RLock()
-		Log("get Pods invoked with Pod count :%d \n", len(PodItemsCache))
+		Log("apiproxy::pods::get Pods invoked with Pod count :%d \n", len(PodItemsCache))
 		podsResponse["items"] = make([]PodItem, 0, len(PodItemsCache))
 		for _, podItem := range PodItemsCache {
 			podsResponse["items"] = append(podsResponse["items"], podItem)
 		}
 		PodCacheRWLock.RUnlock()
+		start := time.Now()
 		j, _ := json.Marshal(podsResponse)
+		elapsed := time.Since(start)
+		Log("apiproxy::pods::Time taken for JSON serilization (milliseconds) :%.2f \n", elapsed.Milliseconds())
 		w.Write(j)
 	default:
 		w.WriteHeader(http.StatusMethodNotAllowed)
@@ -297,7 +347,7 @@ func main() {
 	var err error
 
 	// path MUST be empty for incluster scenario
-	var kubeconfigPath = "" //"/home/sshadmin/testakscluster/testakscluster"
+	var kubeconfigPath = "/home/sshadmin/gangams-aks-hyperscale-test/gangams-aks-hyperscale-test"
 
 	ClientSet, err = createClient(kubeconfigPath)
 	if err != nil {
@@ -305,11 +355,11 @@ func main() {
 		os.Exit(1)
 	}
 
-	go listAndWatchServices()
+	//go listAndWatchServices()
 	go listAndWatchPods()
 
 	http.HandleFunc("/pods", pods)
-	http.HandleFunc("/services", services)
+	//http.HandleFunc("/services", services)
 
 	Log("Starting Kuberenets Proxy Server")
 	http.ListenAndServe(":8080", nil)
