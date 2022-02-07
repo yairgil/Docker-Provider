5481e4870e4fadb5a315d5426266efbef7322767
Ganga Mahesh Siddem <gangams@microsoft.com>
2022-02-07 08:26:36 -0800

    bug fixes

--- source/plugins/ruby/in_kube_podmdminventory.rb
+++ source/plugins/ruby/in_kube_podmdminventory.rb
@@ -70,60 +70,61 @@ module Fluent::Plugin
     def parse_and_emit_records(batchTime = Time.utc.iso8601)
       begin
         if File.exists?(Constants::MDM_POD_INVENTORY_STATE_FILE)
-          content = File.read(Constants::MDM_POD_INVENTORY_STATE_FILE)
-          if !content.empty?
-            $log.info "in_kube_podmdminventory:parse_and_emit_records:Start:Parsing MDM pod records using yajl @ #{Time.now.utc.iso8601}"
-            mdmPodRecords = Yajl::Parser.parse(StringIO.new(content))
-            $log.info "in_kube_podmdminventory:parse_and_emit_records:End:Parsing MDM pod records using yajl @ #{Time.now.utc.iso8601}"
-            if !mdmPodRecords.nil? && !mdmPodRecords.empty?
-              mdmPodRecords.each do |record|
-                @inventoryToMdmConvertor.process_pod_inventory_record(record)
-                @inventoryToMdmConvertor.process_record_for_pods_ready_metric(record["ControllerName"], record["Namespace"], record["PodReadyCondition"])
-                containeRecords = record["containeRecords"]
-                if !containeRecords.nil? && !containeRecords.empty? && containeRecords.length > 0
-                  containeRecords.each do |containerRecord|
-                    if !containerRecord["state"].nil? && !containerRecord["state"].empty?
-                      @inventoryToMdmConvertor.process_record_for_terminated_job_metric(record["ControllerName"], record["Namespace"], containerRecord["state"])
-                    end
-                    begin
-                      if !containerRecord["lastState"].nil? && containerRecord["lastState"].keys.length == 1
-                        lastStateName = containerRecord["lastState"].keys[0]
-                        lastStateObject = containerRecord["lastState"][lastStateName]
-                        if !lastStateObject.is_a?(Hash)
-                          raise "expected a hash object. This could signify a bug or a kubernetes API change"
-                        end
-                        if lastStateObject.key?("reason") && lastStateObject.key?("startedAt") && lastStateObject.key?("finishedAt")
-                          lastStateReason = lastStateObject["reason"]
-                          lastFinishedTime = lastStateObject["finishedAt"]
-                          #Populate mdm metric for OOMKilled container count if lastStateReason is OOMKilled
-                          if lastStateReason.downcase == Constants::REASON_OOM_KILLED
-                            @inventoryToMdmConvertor.process_record_for_oom_killed_metric(record["ControllerName"], record["Namespace"], lastFinishedTime)
-                          end
-                          lastStateReason = nil
-                        end
+          file = File.open(Constants::MDM_POD_INVENTORY_STATE_FILE, "r")
+          $log.info "in_kube_podmdminventory:parse_and_emit_records:Start:Parsing MDM pod records using yajl @ #{Time.now.utc.iso8601}"
+          mdmPodRecords = Yajl::Parser.parse(file)
+          if !file.nil?
+            file.close
+          end
+          $log.info "in_kube_podmdminventory:parse_and_emit_records:End:Parsing MDM pod records using yajl @ #{Time.now.utc.iso8601}"
+          if !mdmPodRecords.nil? && !mdmPodRecords.empty?
+            mdmPodRecords.each do |record|
+              @inventoryToMdmConvertor.process_pod_inventory_record(record)
+              @inventoryToMdmConvertor.process_record_for_pods_ready_metric(record["ControllerName"], record["Namespace"], record["PodReadyCondition"])
+              containeRecords = record["containeRecords"]
+              if !containeRecords.nil? && !containeRecords.empty? && containeRecords.length > 0
+                containeRecords.each do |containerRecord|
+                  if !containerRecord["state"].nil? && !containerRecord["state"].empty?
+                    @inventoryToMdmConvertor.process_record_for_terminated_job_metric(record["ControllerName"], record["Namespace"], containerRecord["state"])
+                  end
+                  begin
+                    if !containerRecord["lastState"].nil? && containerRecord["lastState"].keys.length == 1
+                      lastStateName = containerRecord["lastState"].keys[0]
+                      lastStateObject = containerRecord["lastState"][lastStateName]
+                      if !lastStateObject.is_a?(Hash)
+                        raise "expected a hash object. This could signify a bug or a kubernetes API change"
                       end
-                      containerRestartCount = containerRecord["restartCount"]
-                      #Populate mdm metric for container restart count if greater than 0
-                      if (!containerRestartCount.nil? && (containerRestartCount.is_a? Integer) && containerRestartCount > 0)
-                        @inventoryToMdmConvertor.process_record_for_container_restarts_metric(record["ControllerName"], record["Namespace"], lastFinishedTime)
+                      if lastStateObject.key?("reason") && lastStateObject.key?("startedAt") && lastStateObject.key?("finishedAt")
+                        lastStateReason = lastStateObject["reason"]
+                        lastFinishedTime = lastStateObject["finishedAt"]
+                        #Populate mdm metric for OOMKilled container count if lastStateReason is OOMKilled
+                        if lastStateReason.downcase == Constants::REASON_OOM_KILLED
+                          @inventoryToMdmConvertor.process_record_for_oom_killed_metric(record["ControllerName"], record["Namespace"], lastFinishedTime)
+                        end
+                        lastStateReason = nil
                       end
-                    rescue => err
-                      $log.warn "in_kube_podmdminventory:parse_and_emit_records: failed while processing ContainerLastStatus: #{err}"
-                      $log.debug_backtrace(err.backtrace)
-                      ApplicationInsightsUtility.sendExceptionTelemetry(err)
                     end
+                    containerRestartCount = containerRecord["restartCount"]
+                    #Populate mdm metric for container restart count if greater than 0
+                    if (!containerRestartCount.nil? && (containerRestartCount.is_a? Integer) && containerRestartCount > 0)
+                      @inventoryToMdmConvertor.process_record_for_container_restarts_metric(record["ControllerName"], record["Namespace"], lastFinishedTime)
+                    end
+                  rescue => err
+                    $log.warn "in_kube_podmdminventory:parse_and_emit_records: failed while processing ContainerLastStatus: #{err}"
+                    $log.debug_backtrace(err.backtrace)
+                    ApplicationInsightsUtility.sendExceptionTelemetry(err)
                   end
                 end
               end
-              @log.info "in_kube_podmdminventory:parse_and_emit_records:Sending pod inventory mdm records to out_mdm"
-              pod_inventory_mdm_records = @inventoryToMdmConvertor.get_pod_inventory_mdm_records(batchTime)
-              @log.info "in_kube_podmdminventory:parse_and_emit_records:pod_inventory_mdm_records.size #{pod_inventory_mdm_records.size}"
-              mdm_pod_inventory_es = Fluent::MultiEventStream.new
-              pod_inventory_mdm_records.each { |pod_inventory_mdm_record|
-                mdm_pod_inventory_es.add(batchTime, pod_inventory_mdm_record) if pod_inventory_mdm_record
-              } if pod_inventory_mdm_records
-              router.emit_stream(@@MDMKubePodInventoryTag, mdm_pod_inventory_es) if mdm_pod_inventory_es
             end
+            @log.info "in_kube_podmdminventory:parse_and_emit_records:Sending pod inventory mdm records to out_mdm"
+            pod_inventory_mdm_records = @inventoryToMdmConvertor.get_pod_inventory_mdm_records(batchTime)
+            @log.info "in_kube_podmdminventory:parse_and_emit_records:pod_inventory_mdm_records.size #{pod_inventory_mdm_records.size}"
+            mdm_pod_inventory_es = Fluent::MultiEventStream.new
+            pod_inventory_mdm_records.each { |pod_inventory_mdm_record|
+              mdm_pod_inventory_es.add(batchTime, pod_inventory_mdm_record) if pod_inventory_mdm_record
+            } if pod_inventory_mdm_records
+            router.emit_stream(@@MDMKubePodInventoryTag, mdm_pod_inventory_es) if mdm_pod_inventory_es
           end
         else
           $log.warn "in_kube_podmdminventory:parse_and_emit_records:MDM pod inventory state file doesnt exist @ #{Time.now.utc.iso8601}"
@@ -132,6 +133,10 @@ module Fluent::Plugin
         $log.warn "in_kube_podmdminventory:parse_and_emit_records: failed with an error #{errorStr}"
         $log.debug_backtrace(errorStr.backtrace)
         ApplicationInsightsUtility.sendExceptionTelemetry(errorStr)
+      ensure
+        if !file.nil?
+          file.close
+        end
       end
     end
 
