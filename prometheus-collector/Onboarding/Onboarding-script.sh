d2f78c634a146fb85df70304d409bee84d333679
Rashmi Chandrashekar <rashmy@microsoft.com>
2022-03-26 11:46:32 -0700

    support for cross sub/rg

--- prometheus-collector/Onboarding/Onboarding-script.sh
+++ prometheus-collector/Onboarding/Onboarding-script.sh
@@ -16,9 +16,12 @@
 # For example:
 #
 # bash Onboarding-script.sh "0e4773a2-8221-441a-a06f-17db16ab16d4" "rashmi-canary-template" "rashmi-canary-script-3" "rashmi-canary-grafana-2" "eastus2euap" "/subscriptions/0e4773a2-8221-441a-a06f-17db16ab16d4/resourcegroups/rashmi-canary-template/providers/Microsoft.ContainerService/managedClusters/rashmi-canary-template"
-
+#
 # Cross rg for aks resource -
 # bash Onboarding-script.sh "0e4773a2-8221-441a-a06f-17db16ab16d4" "rashmi-canary-template" "rashmi-canary-script-3" "rashmi-canary-grafana-2" "eastus2euap" "/subscriptions/0e4773a2-8221-441a-a06f-17db16ab16d4/resourcegroups/rashmi-canary-template-2/providers/Microsoft.ContainerService/managedClusters/rashmi-canary-template-2"
+#
+# Cross sub for aks resource -
+# bash Onboarding-script.sh "0e4773a2-8221-441a-a06f-17db16ab16d4" "rashmi-canary-template" "rashmi-canary-script-3" "rashmi-canary-grafana-2" "eastus2euap" /subscriptions/8f6da2d9-ff10-4800-9239-c7e0e8b3407f/resourcegroups/rashmi-canary-10/providers/Microsoft.ContainerService/managedClusters/rashmi-canary-10
 echo "subscriptionId"= ${1}
 echo "resourceGroup" = ${2}
 echo "monitoringAccountName"= ${3}
@@ -38,7 +41,7 @@ aksResourceId=${6}
 macAccountLength=${#3}
 
 # Checking for length of MAC name, since it is used in dc artifacts creation (max is 44)
-if [ $macAccountLength -gt 35 ]
+if [ $macAccountLength -gt 24 ]
 then
     echo "Monitoring account name is longer than 35 characters, please use a shorter name, exiting."
     exit 1
@@ -52,6 +55,21 @@ then
     exit 1
 fi
 
+aksResourceSplitarray=($(echo $aksResourceId | tr "/" "\n"))
+aksResourceIdLength=${#aksResourceSplitarray[@]}
+
+if [ $aksResourceIdLength != 8 ]
+then
+    echo "Incorrect AKS Resource ID specified, please specify an id in this format - /subscriptions/00000000-0000-0000-0000-000000000000/resourcegroups/rg-name/providers/Microsoft.ContainerService/managedClusters/clustername"
+    exit 1
+fi
+
+echo "Getting AKS Subscription id and resource group for DCR association..."
+aksSubId=${aksResourceSplitarray[1]}
+aksRgName=${aksResourceSplitarray[3]}
+echo "AKSSubId: $aksSubId"
+echo "AKSRg: $aksRgName"
+
 #az login
 az extension add -n amg
 az account set -s $subscriptionId
@@ -63,23 +81,25 @@ az grafana create -g $resourceGroup -n $grafanaName
 
 grafanaSmsi=$(az grafana show -g $resourceGroup -n $grafanaName --query 'identity.principalId')
 echo "Got System Assigned Identity for Grafana instance: $grafanaSmsi"
-echo "removing quotes"
+echo "Removing quotes from MSI"
 grafanaSmsi=$(sed -e 's/^"//' -e 's/"$//' <<<"$grafanaSmsi")
 
 #Template to create all resources required for MAC ingestion e2e
 echo "Creating all resources required for MAC ingestion"
-az deployment group create --resource-group $resourceGroup --template-file RootTemplate.json --parameters monitoringAccountName=$monitoringAccountName monitoringAccountLocation=$trimmedLocation targetAKSResource=$aksResourceId
+az deployment group create --resource-group $resourceGroup --template-file RootTemplate.json \
+--parameters monitoringAccountName=$monitoringAccountName monitoringAccountLocation=$trimmedLocation \
+targetAKSResource=$aksResourceId AKSSubId=$aksSubId AKSRg=$aksRgName
 
 macId=$(az resource show -g $resourceGroup -n $monitoringAccountName --resource-type "Microsoft.Monitor/Accounts" --query 'id')
 echo "Got MAC id: $macId"
-echo "removing quotes"
+echo "Removing quotes from MAC Id"
 macId=$(sed -e 's/^"//' -e 's/"$//' <<<"$macId")
 
 echo "Assigning MAC reader role to grafana's system assigned MSI"
 az role assignment create --assignee-object-id $grafanaSmsi --assignee-principal-type ServicePrincipal --scope $macId --role "Monitoring Data Reader MAC"
 
 promQLEndpoint=$(az resource show -g $resourceGroup -n $monitoringAccountName --resource-type "Microsoft.Monitor/Accounts" --query 'properties.metrics.prometheusQueryEndpoint')
-echo "promQLEndpoint: $promQLEndpoint"
+echo "PromQLEndpoint: $promQLEndpoint"
 
 macPromDataSourceConfig='{
     "id": 6,
