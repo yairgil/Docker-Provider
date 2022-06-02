d25919be79291a4cc8d1546d0e8c1d2908f97691
Ganga Mahesh Siddem <gangams@microsoft.com>
2022-06-01 18:40:44 -0700

    avoid imds token call during start up

--- source/plugins/ruby/out_mdm.rb
+++ source/plugins/ruby/out_mdm.rb
@@ -118,7 +118,6 @@ module Fluent::Plugin
               @useMsi = true
               msi_endpoint = @@imds_msi_endpoint_template % { resource: @@token_resource_audience }
               @parsed_token_uri = URI.parse(msi_endpoint)
-              @cached_access_token = get_access_token
             else
               # switch to IMDS endpoint for the windows once the Arc K8s team supports the IMDS sidecar for windows
               @log.info "using cluster identity token since cluster is azure arc k8s cluster"
@@ -148,8 +147,6 @@ module Fluent::Plugin
               end
               @parsed_token_uri = URI.parse(msi_endpoint)
             end
-
-            @cached_access_token = get_access_token
           end
         end
       rescue => e
